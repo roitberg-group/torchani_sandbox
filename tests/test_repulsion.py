9b10b9441b2dd40645a0d0cfb7d0b10bba11ce79
ipickering <ign.pickering@gmail.com>
2021-03-24 17:08:52 -0400

    add repulsion calculator and tests

---
+++ tests/test_repulsion.py
@@ -0,0 +1,95 @@
+import unittest
+import torch
+import torchani
+from torchani.testing import TestCase
+from torchani.repulsion import RepulsionCalculator
+import pickle
+
+def load_model(path, aev_dim):
+    H_network = torch.nn.Sequential(
+        torch.nn.Linear(aev_dim, 160, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(160, 128, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(128, 96, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(96, 1, bias=False)
+    )
+    C_network = torch.nn.Sequential(
+        torch.nn.Linear(aev_dim, 144, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(144, 112, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(112, 96, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(96, 1, bias=False)
+    )
+    N_network = torch.nn.Sequential(
+        torch.nn.Linear(aev_dim, 128, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(128, 112, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(112, 96, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(96, 1, bias=False)
+    )
+    O_network = torch.nn.Sequential(
+        torch.nn.Linear(aev_dim, 128, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(128, 112, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(112, 96, bias=False),
+        torch.nn.GELU(),
+        torch.nn.Linear(96, 1, bias=False)
+    )
+    nn = torchani.ANIModel([H_network, C_network, N_network, O_network])
+    nn.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+    return nn
+
+
+class TestRepulsion(TestCase):
+
+
+    def testRepulsionCalculator(self):
+        rep = RepulsionCalculator(5.2)
+        atom_index12 = torch.tensor([[0], [1]])
+        distances = torch.tensor([3.5])
+        species = torch.tensor([[0, 0]])
+        energies = torch.tensor([0.0])
+        energies = rep((species, energies), atom_index12, distances).energies
+        self.assertTrue(torch.isclose(torch.tensor(3.5325e-08), energies))
+
+    def testRepulsionLongDistances(self):
+        rep = RepulsionCalculator(5.2)
+        atom_index12 = torch.tensor([[0], [1]])
+        distances = torch.tensor([6.0])
+        species = torch.tensor([[0, 0]])
+        energies = torch.tensor([0.0])
+        energies = rep((species, energies), atom_index12, distances).energies
+        self.assertTrue(torch.isclose(torch.tensor(0.0), energies))
+
+    def testRepulsionEnergy(self):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = torchani.models.ANI1x(repulsion=True, model_index=0)
+        model.neural_networks = load_model('repulsion_model_1x.pt', model.aev_computer.aev_length )
+        model.energy_shifter = torchani.EnergyShifter([-0.506930115400, -37.814410115700, -54.55653828400, -75.02918133970])
+        model = model.to(device=device, dtype=torch.double)
+        
+        species = torch.tensor([[3, 0, 0]], device=device)
+        energies=[]
+        distances = torch.linspace(0.1, 6.0, 100)
+        for d in distances:
+            coordinates = torch.tensor([[[0.0, 0.0, 0.0],
+                                        [0.97, 0.0, 0.0],
+                                        [-0.250380004 * d, 0.96814764 * d, 0.0]]],
+                                       requires_grad=True, device=device, dtype=torch.double)
+            energies.append(model((species, coordinates)).energies.item())
+        
+        energies = torch.tensor(energies)
+        with open('energies.pkl', 'rb') as f:
+            energies_salva = torch.tensor(pickle.load(f))
+        self.assertTrue(torch.isclose(energies_salva, energies).all())
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ torchani/repulsion.py
@@ -0,0 +1,70 @@
+from typing import Tuple
+
+import torch
+from torch import Tensor
+
+from . import units
+from .nn import SpeciesEnergies
+from .aev import CutoffSmooth
+
+class RepulsionCalculator(torch.nn.Module):
+    """Calculates the QMDFF repulsion energy terms for a given molecule
+    as seen in work by Grimme: https://pubs.acs.org/doi/10.1021/acs.jctc.8b01176"""
+
+    def __init__(self, Rcr, alpha_buf=None, yeff_buf=None, krep_buf=None, num_species=4, cutoff_function=CutoffSmooth):
+
+        super().__init__()
+        # cutoff distance in Bohr radii for consistency with repulsion parameters
+        Rcr_bohr = Rcr * units.ANGSTROM_TO_BOHR
+        self.register_buffer('Rcr_bohr',torch.tensor(Rcr_bohr))
+        # parameter values for each kind of atom
+        Vyeff=torch.tensor([1.105388, 4.231078, 5.242592, 5.784415])
+        Valpha=torch.tensor([2.213717, 1.247655, 1.682689, 2.165712])
+        # krep for each pair is k=1.5-kA*kB*0.5 (1.5 or 1)
+        Vkrep=torch.tensor([1., 0., 0., 0.])  
+        # matrix of combinations of parameters (pre-calculates constants)
+        y_ab = torch.outer(Vyeff, Vyeff)
+        sqrt_alpha_ab = torch.sqrt(torch.outer(Valpha, Valpha))
+        k_rep_ab = 1.5 - torch.outer(Vkrep, Vkrep) * 0.5
+
+        self.register_buffer('y_ab', y_ab)
+        self.register_buffer('sqrt_alpha_ab', sqrt_alpha_ab)
+        self.register_buffer('k_rep_ab', k_rep_ab)
+
+        self.cutoff_function = cutoff_function(Rcr_bohr) if cutoff_function is not None else None
+
+    def forward(self, species_energies: Tensor, atom_index12: Tensor, distances: Tensor) -> Tuple[Tensor, Tensor]:
+
+        species, energies = species_energies
+        assert len(species) == 1, "Not implemented for batch calculations"
+        assert distances.ndim == 1, "distances should be 1 dimensional"
+        assert species.ndim == 2, "species_energies should be 2 dimensional"
+        assert atom_index12.ndim == 2, "atom_index12 should be 2 dimensional"
+        assert len(distances) == atom_index12.shape[1]
+
+        # distances has all interaction pairs within a given cutoff, 
+        # for a molecule or set of molecules
+        # and atom_index12 holds all pairs of indices
+        # species is of shape (C x Atoms)
+        species12 = species.flatten()[atom_index12]
+
+        # distances need to be in Bohr radii units (cutoff distance too for coherence)
+        distances_bohr = distances * units.ANGSTROM_TO_BOHR
+
+        # find pre-computed constant multiplications for every species pair
+        y_ab = self.y_ab[species12[0], species12[1]]
+        sqrt_alpha_ab = self.sqrt_alpha_ab[species12[0], species12[1]]
+        k_rep_ab = self.k_rep_ab[species12[0], species12[1]]
+
+        # calculates repulsion energies using distances and constants 
+        # NOTE: for batch implementation we should add in only one dimension
+        prefactor = (y_ab / distances_bohr)
+        repulsion_energy =  prefactor * torch.exp(-sqrt_alpha_ab * (distances_bohr ** k_rep_ab))
+
+        if self.cutoff_function is not None:
+            repulsion_energy *= self.cutoff_function(distances_bohr)
+
+        repulsion_energy = repulsion_energy.sum()
+        
+        energies += repulsion_energy
+        return SpeciesEnergies(species, energies)
