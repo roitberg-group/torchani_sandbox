bae7fd1b5778fb8cefebb6ed680f65e317130562
ipickering <ign.pickering@gmail.com>
2021-03-25 18:44:09 -0400

    improve testing coverage

--- tests/test_aev_benzene_md.py
+++ tests/test_aev_benzene_md.py
@@ -3,6 +3,7 @@ import torch
 import pickle
 import torchani
 import unittest
+from torchani.aev import CellList
 from common_aev_test import _TestAEVBase
 
 path = os.path.dirname(os.path.realpath(__file__))
@@ -13,6 +14,23 @@ class TestAEVBenzeneMD(_TestAEVBase):
     def testBenzeneMD(self):
         for i in [2, 8]:
             datafile = os.path.join(path, 'test_data/benzene-md/{}.dat'.format(i))
+            with open(datafile, 'rb') as f:
+                coordinates, species, expected_radial, expected_angular, _, _, cell, pbc \
+                    = pickle.load(f)
+                coordinates = torch.from_numpy(coordinates).float().unsqueeze(0)
+                species = torch.from_numpy(species).unsqueeze(0)
+                expected_radial = torch.from_numpy(expected_radial).float().unsqueeze(0)
+                expected_angular = torch.from_numpy(expected_angular).float().unsqueeze(0)
+                cell = torch.from_numpy(cell).float()
+                pbc = torch.from_numpy(pbc)
+                #coordinates = torchani.utils.map_to_central(coordinates, cell, pbc)
+                _, aev = self.aev_computer((species, coordinates), cell=cell, pbc=pbc)
+                self.assertAEVEqual(expected_radial, expected_angular, aev)
+
+    def testBenzeneMDCellList(self):
+        for i in [2, 8]:
+            datafile = os.path.join(path, 'test_data/benzene-md/{}.dat'.format(i))
+            self.aev_computer.neighborlist = CellList(cutoff=self.aev_computer.radial_terms.cutoff)
             with open(datafile, 'rb') as f:
                 coordinates, species, expected_radial, expected_angular, _, _, cell, pbc \
                     = pickle.load(f)
