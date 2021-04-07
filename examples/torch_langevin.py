cda22f827413dd700d94ab77e975c8f0e0651212
ipickering <ign.pickering@gmail.com>
2021-04-07 17:48:22 -0400

    improve a bit example

--- examples/torch_langevin.py
+++ examples/torch_langevin.py
@@ -1,6 +1,7 @@
 import torch
 from torchani.md import TorchLangevin
 import torchani
+import time
 
 # THis class runs dynamics fully on the GPU, without needing to send tensors to the
 # CPU every step, like when using the ASE interface
@@ -15,10 +16,13 @@ coordinates = torch.tensor([[[0.03192167, 0.00638559, 0.01301679],
                              [0.66091919, -0.16799635, -0.91037834]]],
                            requires_grad=True, device=device).double()
 species = torch.tensor([[6, 1, 1, 1, 1]], device=device)
-coordinates = coordinates.repeat(2, 1, 1)
-species = species.repeat(2, 1)
+r = 3
+coordinates = coordinates.repeat(r, 1, 1)
+species = species.repeat(r, 1)
 
 # T in K, gamma in 1/fs, timestep in fs
 dyn = TorchLangevin(model, species, coordinates, 300, 0.02, 0.1)
-
-dyn.run(10000, print_every=10)
+start = time.time()
+dyn.run(100, print_every=10)
+end = time.time()
+print(end - start)
