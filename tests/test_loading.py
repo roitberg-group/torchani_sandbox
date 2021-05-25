c8510846e9296e17190a719e7d3c78aa5e28ee44
ipickering <ign.pickering@gmail.com>
2021-05-25 00:04:01 -0400

    test for state dict equality and all 1x single models

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -10,20 +10,23 @@ class TestLoading(TestCase):
     def testANI1x(self):
         model = torchani.models.ANI1x(periodic_table_index=True, use_neurochem_source=False)
         model_nc = torchani.models.ANI1x(periodic_table_index=True, use_neurochem_source=True)
-        for p1, p2 in zip(model_nc.state_dict(), model.state_dict()):
-            self.assertEqual(p1, p2)
+        self.assertEqual(model_nc.state_dict(), model.state_dict())
+
+    def testANI1xSingle(self):
+        for j in range(8):
+            model = torchani.models.ANI1x(periodic_table_index=True, model_index=j, use_neurochem_source=False)
+            model_nc = torchani.models.ANI1x(periodic_table_index=True, model_index=j, use_neurochem_source=True)
+            self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI2x(self):
         model = torchani.models.ANI2x(periodic_table_index=True, use_neurochem_source=False)
         model_nc = torchani.models.ANI2x(periodic_table_index=True, use_neurochem_source=True)
-        for p1, p2 in zip(model_nc.parameters(), model.parameters()):
-            self.assertEqual(p1, p2)
+        self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI1ccx(self):
         model = torchani.models.ANI1ccx(periodic_table_index=True, use_neurochem_source=False)
         model_nc = torchani.models.ANI1ccx(periodic_table_index=True, use_neurochem_source=True)
-        for p1, p2 in zip(model_nc.parameters(), model.parameters()):
-            self.assertEqual(p1, p2)
+        self.assertEqual(model_nc.state_dict(), model.state_dict())
 
 
 if __name__ == '__main__':
