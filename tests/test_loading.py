74ac1450d49bb156e1e3a4e1994302e6fa114e82
Ignacio Pickering <ign.pickering@gmail.com>
2021-05-24 22:14:07 -0400

    Update tests/test_loading.py
    Co-authored-by: Jinze (Richard) Xue <yueyericardo@gmail.com>

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -10,7 +10,7 @@ class TestLoading(TestCase):
     def testANI1x(self):
         model = torchani.models.ANI1x(periodic_table_index=True, use_neurochem_source=False)
         model_nc = torchani.models.ANI1x(periodic_table_index=True, use_neurochem_source=True)
-        for p1, p2 in zip(model_nc.parameters(), model.parameters()):
+        for p1, p2 in zip(model_nc.state_dict(), model.state_dict()):
             self.assertEqual(p1, p2)
 
     def testANI2x(self):
