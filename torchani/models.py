b3191f5d830f25642ef6e2ed2061fb2f773a66bb
ipickering <ign.pickering@gmail.com>
2021-05-27 23:02:57 -0400

    mypy

--- torchani/models.py
+++ torchani/models.py
@@ -34,7 +34,6 @@ from pathlib import Path
 from collections import OrderedDict
 import torch
 from torch import Tensor
-from torch.nn import Module
 from typing import Tuple, Optional, NamedTuple, Sequence, Union, Type
 from .nn import SpeciesConverter, SpeciesEnergies, Ensemble, ANIModel
 from .utils import ChemicalSymbolsToInts, PERIODIC_TABLE, EnergyShifter, path_is_writable
@@ -52,7 +51,7 @@ class SpeciesEnergiesQBC(NamedTuple):
     qbcs: Tensor
 
 
-class BuiltinModel(Module):
+class BuiltinModel(torch.nn.Module):
     r"""Private template for the builtin ANI models """
 
     atomic_numbers: Tensor
@@ -356,7 +355,7 @@ class BuiltinModelExternalInterface(BuiltinModel):
 def _get_component_modules(state_dict_file: str,
                            model_index: Optional[int] = None,
                            use_cuda_extension: bool = False,
-                           ensemble_size: int = 8) -> Tuple[AEVComputer, Module, EnergyShifter, Sequence[str]]:
+                           ensemble_size: int = 8) -> Tuple[AEVComputer, NN, EnergyShifter, Sequence[str]]:
     # This generates ani-style architectures without neurochem
     name = state_dict_file.split('_')[0]
     elements: Tuple[str, ...]
