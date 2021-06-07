9d06240c49b1e4d4c2323daefe4197951406dc9a
ipickering <ign.pickering@gmail.com>
2021-06-07 01:04:40 -0400

    make builtin datasets visible

--- torchani/datasets/__init__.py
+++ torchani/datasets/__init__.py
@@ -1,4 +1,4 @@
-from .datasets import AniBatchedDataset, AniH5Dataset, AniH5DatasetList, create_batched_dataset
+from .datasets import AniBatchedDataset, AniH5Dataset, AniH5DatasetList, create_batched_dataset, RawANI1x, RawANI2x
 from . import utils
 
-__all__ = ['AniBatchedDataset', 'AniH5Dataset', 'AniH5DatasetList', 'create_batched_dataset', 'utils']
+__all__ = ['AniBatchedDataset', 'AniH5Dataset', 'AniH5DatasetList', 'create_batched_dataset', 'utils', 'RawANI1x', 'RawANI2x']
