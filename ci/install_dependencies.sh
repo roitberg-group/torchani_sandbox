d1bfd035a41de4f3a2d3110eb23ad5cdf9637ea2
ipickering <ign.pickering@gmail.com>
2021-06-07 03:56:09 -0400

    fix workflow

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install --upgrade pip
-pip install twine wheel
-pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html
-pip install -r test_requirements.txt
-pip install -r docs_requirements.txt
+pip install --no-cache-dir twine wheel
+pip install --no-cache-dir --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html
+pip install --no-cache-dir -r test_requirements.txt
+pip install --no-cache-dir -r docs_requirements.txt
