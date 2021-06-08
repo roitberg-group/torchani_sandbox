5d46b76597193c6418aa64d1e7cac032fa77f603
ipickering <ign.pickering@gmail.com>
2021-06-08 03:11:26 -0400

    enforce order of files

--- torchani/datasets/datasets.py
+++ torchani/datasets/datasets.py
@@ -313,11 +313,12 @@ class _BaseBuiltinRawDataset(AniH5DatasetList):
     def __init__(self, root: Union[str, Path],
                        download: bool = False,
                        archive: Optional[str] = None,
-                       files_and_md5s: Optional[Dict[str, str]] = None,
+                       files_and_md5s: Optional['OrderedDict[str, str]'] = None,
                        **h5_dataset_list_kwargs):
+        assert isinstance(files_and_md5s, OrderedDict)
 
         self._archive: str = '' if archive is None else archive
-        self._files_and_md5s: Dict[str, str] = dict() if files_and_md5s is None else files_and_md5s
+        self._files_and_md5s = OrderedDict([('', '')]) if files_and_md5s is None else files_and_md5s
 
         root = Path(root).resolve()
         if download:
@@ -328,8 +329,13 @@ class _BaseBuiltinRawDataset(AniH5DatasetList):
             if not self._check_hdf5_files_integrity(root):
                 raise RuntimeError('Dataset not found or is corrupted, '
                                    'you can use "download = True" to download it')
+        dataset_paths = [Path(p).resolve() for p in list_files(root, suffix='.h5', prefix=True)]
 
-        dataset_paths = list_files(root, suffix='.h5', prefix=True)
+        # Order dataset paths using the order given in "files and md5s"
+        filenames_order = {k: j for j, k in enumerate(self._files_and_md5s.keys())}
+        dataset_filenames_and_paths = sorted([(p.name, p) for p in dataset_paths], key=lambda tup: filenames_order[tup[0]])
+        dataset_paths = [p for _, p in dataset_filenames_and_paths]
+
         super().__init__(dataset_paths, flag_property='coordinates', element_keys=('species',), **h5_dataset_list_kwargs)
 
     def _check_hdf5_files_integrity(self, root: Union[str, Path]) -> bool:
@@ -361,7 +367,8 @@ class _BaseBuiltinRawDataset(AniH5DatasetList):
 
 class RawANI1x(_BaseBuiltinRawDataset):
     _ARCHIVE = 'ANI-1x-wB97X-6-31Gd-data.tar.gz'
-    _FILES_AND_MD5S = {'ANI-1x-wB97X-6-31Gd.h5': 'c9d63bdbf90d093db9741c94d9b20972'}
+    # NOTE: The order of this dictionary is important since it deterimenes the order of iteration over the files
+    _FILES_AND_MD5S = OrderedDict([('ANI-1x-wB97X-6-31Gd.h5', 'c9d63bdbf90d093db9741c94d9b20972')])
 
     def __init__(self, root: Union[str, Path], download: bool = False, **base_kwargs):
         super().__init__(root, download, archive=self._ARCHIVE, files_and_md5s=self._FILES_AND_MD5S, **base_kwargs)
@@ -370,9 +377,10 @@ class RawANI1x(_BaseBuiltinRawDataset):
 class RawANI2x(_BaseBuiltinRawDataset):
 
     _ARCHIVE = 'ANI-2x-wB97X-6-31Gd-data.tar.gz'
-    _FILES_AND_MD5S = {'ANI-1x-wB97X-6-31Gd.h5': 'c9d63bdbf90d093db9741c94d9b20972',
-                     'ANI-2x-heavy-wB97X-6-31Gd.h5': '49ec3dc5d046f5718802f5d1f102391c',
-                     'ANI-2x-dimers-wB97X-6-31Gd.h5': '3455d82a50c63c389126b68607fb9ca8'}
+    # NOTE: The order of this dictionary is important since it deterimenes the order of iteration over the files
+    _FILES_AND_MD5S = OrderedDict([('ANI-1x-wB97X-6-31Gd.h5', 'c9d63bdbf90d093db9741c94d9b20972'),
+                                   ('ANI-2x-heavy-wB97X-6-31Gd.h5', '49ec3dc5d046f5718802f5d1f102391c'),
+                                   ('ANI-2x-dimers-wB97X-6-31Gd.h5', '3455d82a50c63c389126b68607fb9ca8')])
 
     def __init__(self, root: Union[str, Path], download: bool = False, **base_kwargs):
         super().__init__(root, download, archive=self._ARCHIVE, files_and_md5s=self._FILES_AND_MD5S, **base_kwargs)
@@ -380,18 +388,19 @@ class RawANI2x(_BaseBuiltinRawDataset):
 
 class RawCOMP6v1(_BaseBuiltinRawDataset):
     _ARCHIVE = 'COMP6-v1-data.tar.gz'
-    _FILES_AND_MD5S = {'GDB11-07-test-500.h5': '9200755bfc755405e64100a53a9f7468',
-                       'GDB11-08-test-500.h5': '202b078f98a911a7a9bdc21ee0ae1af7',
-                       'GDB11-09-test-500.h5': '5d2f6573c07e01493e4c7f72edabe483',
-                       'GDB11-10-test-500.h5': '96acd0003f6faeacb51b4db483c1d6f8',
-                       'GDB11-11-test-500.h5': 'b7bf4fa7d2f78b8168f243b1a6aa6071',
-                       'GDB13-12-test-1000.h5': '4317beed9425ee63659e41144475115c',
-                       'GDB13-13-test-1000.h5': '4095ae8981a5e4b10fbc1f29669b0af5',
-                       'DrugBank-Testset.h5': 'fae59730172c7849478271dbf585c8ce',
-                       'DrugBank-Testset-SFCl.h5': 'dca0987a6030feca5b8e9a1e24102b44',
-                       'Tripeptides-Full.h5': 'bb7238f3634217e834b7eee94febc816',
-                       'ANI-MD-Bench.h5': '9e3a1327d01730033edeeebd6fac4d6c',
-                       'S66-x8-wB97X-6-31Gd.h5': 'df1a5f3b9b6599d56f1a78631a83b720'}
+    # NOTE: The order of this dictionary is important since it deterimenes the order of iteration over the files
+    _FILES_AND_MD5S = OrderedDict([('GDB11-07-test-500.h5', '9200755bfc755405e64100a53a9f7468'),
+                                   ('GDB11-08-test-500.h5', '202b078f98a911a7a9bdc21ee0ae1af7'),
+                                   ('GDB11-09-test-500.h5', '5d2f6573c07e01493e4c7f72edabe483'),
+                                   ('GDB11-10-test-500.h5', '96acd0003f6faeacb51b4db483c1d6f8'),
+                                   ('GDB11-11-test-500.h5', 'b7bf4fa7d2f78b8168f243b1a6aa6071'),
+                                   ('GDB13-12-test-1000.h5', '4317beed9425ee63659e41144475115c'),
+                                   ('GDB13-13-test-1000.h5', '4095ae8981a5e4b10fbc1f29669b0af5'),
+                                   ('DrugBank-Testset.h5', 'fae59730172c7849478271dbf585c8ce'),
+                                   ('DrugBank-Testset-SFCl.h5', 'dca0987a6030feca5b8e9a1e24102b44'),
+                                   ('Tripeptides-Full.h5', 'bb7238f3634217e834b7eee94febc816'),
+                                   ('ANI-MD-Bench.h5', '9e3a1327d01730033edeeebd6fac4d6c'),
+                                   ('S66-x8-wB97X-6-31Gd.h5', 'df1a5f3b9b6599d56f1a78631a83b720')])
 
     def __init__(self, root: Union[str, Path], download: bool = False, **base_kwargs):
         super().__init__(root, download, archive=self._ARCHIVE, files_and_md5s=self._FILES_AND_MD5S, **base_kwargs)
