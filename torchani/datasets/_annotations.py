613849900f4cc7222313cfd39891d8a991b1750a
ipickering <ign.pickering@gmail.com>
2021-06-17 04:42:35 -0400

    fix numpy typing

--- torchani/datasets/_annotations.py
+++ torchani/datasets/_annotations.py
@@ -2,11 +2,19 @@ r"""Mypy type aliases"""
 from typing import Tuple, Dict, Union, Callable, TypeVar, Any
 from torch import Tensor
 from pathlib import Path
-from numpy import ndarray
+from numpy import ndarray, dtype
 from collections import OrderedDict
-from numpy import typing as numpy_typing
+import sys
 
-DTypeLike = numpy_typing.DTypeLike
+# This is needed for compatibility with python 3.6, where numpy typing doesn't
+# work correctly
+if sys.version_info[:2] < (3, 7):
+    # This doesn't really matter anyways since it is only for mypy
+    DTypeLike = dtype
+else:
+    from numpy import typing as numpy_typing
+    DTypeLike = numpy_typing.DTypeLike
+
 KeyIdx = Tuple[str, Tensor]
 Transform = Callable[[Dict[str, Tensor]], Dict[str, Tensor]]
 
