ea8427703f4210dd3685d10ff484285a1441d65b
Jinze Xue <yueyericardo@gmail.com>
2021-06-22 12:26:50 -0400

    more py versions

--- conda/build_conda.sh
+++ conda/build_conda.sh
@@ -14,7 +14,7 @@ setup_cuda_home
 setup_conda_pytorch_constraint
 setup_conda_cudatoolkit_constraint
 
-export PYTHON_VERSION="3.8"
 # build
-conda build $CONDA_CHANNEL_FLAGS --no-anaconda-upload --python "$PYTHON_VERSION" "$script_dir/torchani"
+conda build $CONDA_CHANNEL_FLAGS --no-anaconda-upload "$script_dir/torchani"
 # conda install anaconda-client
+# anaconda upload *.tar.bz2
