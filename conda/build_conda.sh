ab5686bb50cb0f5452ba2d2f96462c20070d5242
Jinze Xue <yueyericardo@gmail.com>
2021-06-28 18:17:52 -0400

    save

--- conda/build_conda.sh
+++ conda/build_conda.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ex
 
+# USAGE:
+# 1. test
+# ./build_conda.sh test
+# 2. release
+# ./build_conda.sh release CONDA_TOKEN
+
 # helper functions
 script_dir=$(dirname $(realpath $0))
 . "$script_dir/pkg_helpers.bash"
@@ -14,7 +20,27 @@ setup_cuda_home
 setup_conda_pytorch_constraint
 setup_conda_cudatoolkit_constraint
 
-# build
+if (( $# == 0 )); then
+    >&2 echo "Illegal number of parameters"
+    exit 1
+fi
+
+# conda-build dependency
 conda install conda-build conda-verify anaconda-client -y
-anaconda -t ${{ secrets.CONDA_TOKEN }}
-conda build $CONDA_CHANNEL_FLAGS "$script_dir/torchani"
+
+# do not upload
+if [[ $1 == "test" ]]; then
+    conda build $CONDA_CHANNEL_FLAGS --no-anaconda-upload "$script_dir/torchani"
+    echo test
+fi
+
+# upload to anaconda.org
+if [[ $1 == "release" ]]; then
+    if (( $# < 2 )); then
+        >&2 echo "No conda token provided "
+        exit 1
+    fi
+    CONDA_TOKEN=$2
+    anaconda -t $CONDA_TOKEN
+    conda build $CONDA_CHANNEL_FLAGS "$script_dir/torchani"
+fi
