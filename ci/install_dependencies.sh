b629e6b47d841f139fd8ea2a64c920c8d172efe1
Jinze Xue <yueyericardo@gmail.com>
2021-06-29 18:36:01 -0400

    fix libtorch_global_deps.so not found

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -2,6 +2,6 @@
 
 pip install --upgrade pip
 pip install twine wheel
-pip install --no-cache-dir --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html --upgrade
+pip install --no-cache-dir --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html --upgrade --no-deps --force-reinstall
 pip install -r test_requirements.txt
 pip install -r docs_requirements.txt
