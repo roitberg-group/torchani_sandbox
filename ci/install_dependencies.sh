c15081162e9535403a779ddf0a0aa0e396fdf182
Jinze Xue <yueyericardo@gmail.com>
2021-06-30 18:20:09 -0400

    save

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -ex
 
 pip install --upgrade pip
 pip install twine wheel
 # clean pip cache if it's larger than 10GB
 file_size=$(du -bs $(pip cache dir) | cut -f1)
+du -bsh $(pip cache dir)
 if [[ $file_size -gt 10737418240 ]]; then pip cache purge; fi
 pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html --upgrade
 pip install -r test_requirements.txt
