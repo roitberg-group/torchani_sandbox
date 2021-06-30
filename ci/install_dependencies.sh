8f2ac01d9a5d31e83138823052ac6dd23d46be80
Jinze Xue <yueyericardo@gmail.com>
2021-06-30 17:03:15 -0400

    reinstall is too slow

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -5,6 +5,6 @@ pip install twine wheel
 # clean pip cache if it's larger than 10GB
 file_size=$(du -bs $(pip cache dir) | cut -f1)
 if [[ $file_size -gt 10737418240 ]]; then pip cache purge; fi
-pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html --upgrade --no-deps --force-reinstall
+pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html --upgrade
 pip install -r test_requirements.txt
 pip install -r docs_requirements.txt
