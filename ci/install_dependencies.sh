7e1ef5bd3c87d409f79c988f356e0380fe3fe9f1
Jinze Xue <yueyericardo@gmail.com>
2021-06-30 20:01:03 -0400

    upgrade torch nightly every saturday night

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -7,6 +7,6 @@ pip install twine wheel
 file_size=$(du -bs $(pip cache dir) | cut -f1)
 du -bsh $(pip cache dir)
 if [[ $file_size -gt 10737418240 ]]; then pip cache purge; fi
-pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html --upgrade
+pip install --pre torch torchvision -f https://download.pytorch.org/whl/nightly/cu111/torch_nightly.html
 pip install -r test_requirements.txt
 pip install -r docs_requirements.txt
