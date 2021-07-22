9bb3f11dea42bedbab0148b273cfee57a2ed1873
ipickering <ign.pickering@gmail.com>
2021-07-22 11:46:47 -0400

    fix mypy

--- examples/builtin_dataset_example.py
+++ examples/builtin_dataset_example.py
@@ -7,12 +7,12 @@ import torchani
 # perform a checksum to make sure the file's integrity is not compromised. If
 # the function is called again and the dataset is already on the path, only the
 # checksum is performed, the data is not downloaded.
-ds = torchani.datasets.ANI1x('./datasets/ani1x/', download=True)
+ds_1x = torchani.datasets.ANI1x('./datasets/ani1x/', download=True)
 # This constructs an ANIDataset class, so you can iterate over it or
 # perform whatever operation you want.
-for k, v in ds.items():
+for k, v in ds_1x.items():
     print(k, v)
 # The comp6v1 and 2x datasets are also available.
 # (watch out, downloading the datasets may take some time)
-ds = torchani.datasets.COMP6v1('./datasets/comp6v1/', download=True)
-ds = torchani.datasets.ANI2x('./datasets/ani2x/', download=True)
+ds_comp6 = torchani.datasets.COMP6v1('./datasets/comp6v1/', download=True)
+ds_2x = torchani.datasets.ANI2x('./datasets/ani2x/', download=True)
