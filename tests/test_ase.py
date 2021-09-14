2fb5013b54acedc06540206324071a1aa3333f5f
ipickering <ign.pickering@gmail.com>
2021-09-13 20:49:15 -0400

    fix bug

--- tests/test_ase.py
+++ tests/test_ase.py
@@ -77,13 +77,13 @@ class TestASE(TestCase):
         atoms = Diamond(symbol="C", pbc=True, size=(repeats, repeats, repeats))
         calculator = model.ase()
         atoms.calc = calculator
-        dyn = Langevin(atoms, 0.5 * units.fs, 300, 0.002, rng=prng)
+        dyn = Langevin(atoms, timestep=0.5 * units.fs, temperature_K=3820, friction=0.002, rng=prng)
         dyn.run(steps)
-        atoms = Diamond(symbol="C", pbc=True)
-        calculator = self.model.ase()
-        atoms.calc = calculator
-        dyn = Langevin(atoms, 5 * units.fs, temperature_K=3820, friction=0.002)
-        dyn.run(100)
+        #atoms = Diamond(symbol="C", pbc=True)
+        #calculator = model.ase()
+        #atoms.calc = calculator
+        #dyn = Langevin(atoms, 5 * units.fs, temperature_K=3820, friction=0.002)
+        #dyn.run(100)
         f = atoms.get_forces()
         if only_get_forces:
             return f
@@ -126,7 +126,7 @@ class TestASE(TestCase):
         # Note that there are 4 benzene molecules, thus, 48 atoms in
         # Benzene.json
         benzene.set_velocities(np.full((48, 3), 1e-15))
-        calculator = self.model.ase()
+        calculator = model.ase()
         benzene.calc = calculator
         dyn = NPTBerendsen(benzene, timestep=0.1 * units.fs,
                            temperature_K=300,
