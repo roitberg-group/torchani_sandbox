6ebf73e6d64ab13a4076c20707038513e285710f
ipickering <ign.pickering@gmail.com>
2021-09-16 00:16:07 -0400

    add simple training script

---
+++ examples/simple_training.py
@@ -0,0 +1,260 @@
+import time
+import math
+from pathlib import Path
+from pprint import pprint
+from typing import Optional, Union, Dict, Callable
+
+import torch
+from torch import Tensor
+from torch.optim import Optimizer
+import torch.utils.tensorboard
+from torch.optim.lr_scheduler import ReduceLROnPlateau, _LRScheduler
+from tqdm import tqdm
+
+import torchani
+from torchani import datasets, transforms, units
+# Simple training script, bare bones, which uses mypy for extra safety
+
+# Mypy
+DatasetType = Union[torch.utils.data.DataLoader, datasets.AniBatchedDataset]
+DeviceType = Union[int, str, torch.device]
+Transform = Callable[[Dict[str, Tensor]], Dict[str, Tensor]]
+PathLike = Union[str, Path]
+ScalarMetrics = Dict[str, float]
+LRScheduler = Union[ReduceLROnPlateau, _LRScheduler]
+Stateful = Union[torch.nn.Module, torchani.models.BuiltinModel, LRScheduler, Optimizer]
+
+
+# Validation / Training logic, including loss
+class Runner:
+    def __init__(self, model: torchani.models.BuiltinModel,
+                       optimizer: Optimizer,
+                       transform: Optional[Transform] = None,
+                       device: Optional[DeviceType] = None,
+                       best_metric: float = math.inf):
+        self._transform = transform
+        self._device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') if device is None else device
+        self._model = model
+        self._optimizer = optimizer
+        self._mse = torch.nn.MSELoss(reduction='none')
+        # metric to track to check if it improves
+        self.best_metric = best_metric
+        self.best_metric_improved_last_run = False
+
+    def _run(self, dataset: DatasetType,
+                   epoch: Optional[int] = None,
+                   train: bool = False,
+                   use_tqdm: bool = True,
+                   verbose: bool = True) -> ScalarMetrics:
+        # outputs for logging
+        mean_epoch_loss = torch.tensor(0.0, dtype=torch.float)
+        mean_epoch_rmse = torch.tensor(0.0, dtype=torch.float)
+        count = 0
+        self._model.train(train)
+        if use_tqdm:
+            desc = f"epoch {epoch}" if epoch is not None else None
+            batches = tqdm(dataset, total=len(dataset), desc=desc)
+        else:
+            batches = dataset
+
+        for properties in batches:
+            properties = {k: v.to(self._device, non_blocking=True) for k, v in properties.items()}
+            properties = self._transform(properties)
+            species = properties['species'].long()
+            coordinates = properties['coordinates'].float()
+            target_energies = properties['energies'].float()
+            _, predicted_energies = self._model((species, coordinates)).energies
+
+            num_atoms = (species >= 0).sum(dim=1, dtype=predicted_energies.dtype)
+            mse = self._mse(predicted_energies, target_energies)
+            scaled_mse = (mse / num_atoms.sqrt)
+
+            if train:
+                loss = scaled_mse.mean()
+                self._optimizer.zero_grad()
+                loss.backward()
+                self._optimizer.step()
+
+            # update for logging
+            mean_epoch_loss += scaled_mse.detach()
+            mean_epoch_rmse += mse.detach()
+            count += species.shape[0]
+
+        metrics = {'loss_hartree': (mean_epoch_loss / count).item(),
+                   'rmse_kcalpermol': units.hartree2kcalmol(torch.sqrt(mean_epoch_rmse / count)).item()}
+        self._model.train(not train)
+
+        if verbose:
+            if isinstance(dataset, datasets.AniBatchedDataset):
+                split = dataset.split
+            else:
+                assert isinstance(dataset.dataset, datasets.AniBatchedDataset)
+                split = dataset.dataset.split
+            print(f'{split} metrics:')
+            pprint(metrics)
+        return metrics
+
+    def train(self, dataset: DatasetType, epoch: int, **kwargs: bool) -> ScalarMetrics:
+        metrics = self._run(dataset, epoch, train=True, **kwargs)
+        return metrics
+
+    def eval(self, dataset: DatasetType, epoch: int, track_metric: Optional[str] = None, **kwargs: bool) -> ScalarMetrics:
+        with torch.no_grad():
+            metrics = self._run(dataset, epoch, train=False, **kwargs)
+        if track_metric is not None:
+            if metrics[track_metric] < self.best_metric:
+                self.best_metric = metrics[track_metric]
+                self.best_metric_improved_last_run = True
+            metrics.update({f'best_{track_metric}': self.best_metric})
+        return metrics
+
+    def to(self, device: DeviceType) -> 'Runner':
+        self._device = device
+        return self
+
+    def load_state_dict(self, state_dict: ScalarMetrics) -> None:
+        self.__dict__.update(state_dict)
+
+    def state_dict(self) -> ScalarMetrics:
+        return {'best_metric': self.best_metric}
+
+
+class Logger:
+    def __init__(self, path: Optional[PathLike] = None):
+        path = Path(path).resolve() if path is not None else Path('./runs').resolve()
+        self._writer = torch.utils.tensorboard.SummaryWriter(path)
+
+    def log_scalars(self, step: int,
+                    train_metrics: Optional[ScalarMetrics] = None,
+                    validate_metrics: Optional[ScalarMetrics] = None,
+                    other: Optional[ScalarMetrics] = None) -> None:
+        if train_metrics is not None:
+            for k, v in train_metrics.items():
+                self._writer.add_scalar(f'{k}/train', v, step)
+        if validate_metrics is not None:
+            for k, v in validate_metrics.items():
+                self._writer.add_scalar(f'{k}/validate', v, step)
+        if other is not None:
+            for k, v in other.items():
+                self._writer.add_scalar(k, v, step)
+
+
+# Checkpointing
+def _ensure_state_dicts(objects):
+    for v in objects.values():
+        assert hasattr(v, 'state_dict')
+        assert hasattr(v, 'load_state_dict')
+
+
+def save_checkpoint(path: PathLike, objects: Dict[str, Stateful]) -> None:
+    _ensure_state_dicts(objects)
+    torch.save({k: v.state_dict() for k, v in objects.items()}, Path(path).resolve())
+
+
+def load_checkpoint(path: PathLike, objects: Dict[str, Stateful]) -> None:
+    _ensure_state_dicts(objects)
+    checkpoint = torch.load(Path(path).resolve())
+    for k in objects.keys():
+        objects[k].load_state_dict(checkpoint[k])
+
+
+if __name__ == '__main__':
+    # transform, model and runner have to be sent to device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Training and validation sets
+    h5_path = Path('/home/ignacio/Datasets/ani1x_release_wb97x_dz.h5').resolve()
+    batched_dataset_path = Path('./batched_dataset_1x').resolve()
+    if not batched_dataset_path.is_dir():
+        datasets.create_batched_dataset(h5_path,
+                                        dest_path=batched_dataset_path,
+                                        batch_size=2560,
+                                        splits={'training': 0.8, 'validation': 0.2})
+
+    training = torch.utils.data.DataLoader(datasets.AniBatchedDataset(batched_dataset_path, split='training'),
+                                           shuffle=True,
+                                           num_workers=2,
+                                           prefetch_factor=2,
+                                           pin_memory=True,
+                                           batch_size=None)
+
+    validation = torch.utils.data.DataLoader(datasets.AniBatchedDataset(batched_dataset_path, split='validation'),
+                                             shuffle=False,
+                                             num_workers=2,
+                                             prefetch_factor=2,
+                                             pin_memory=True,
+                                             batch_size=None)
+
+    # Model
+    model = torchani.models.ANI1x(pretrained=False, model_index=0, use_cuda_extension=True).to(device)
+    model.subtract_saes_(False)
+    model.periodic_table_index_(False)
+
+    # Transforms
+    elements = model.get_chemical_symbols()
+    self_energies = [-0.499321200000, -37.83383340000, -54.57328250000, -75.04245190000]  # GSAEs
+    transform = transforms.Compose([transforms.AtomicNumbersToIndices(elements), transforms.SubtractSAE(elements, self_energies)]).to(device)
+
+    # Optimizer and lr scheduler
+    initial_lr = 1e-3
+    weight_decay = 1e-7
+    optimizer = torch.optim.AdamW(model.parameters(), weight_decay=weight_decay, lr=initial_lr)
+
+    # Lr scheduler
+    max_epochs = 100
+    early_stopping_learning_rate = 1.0e-6
+    track_metric = 'rmse_kcalpermol'
+    scheduler = ReduceLROnPlateau(optimizer, factor=0.5, patience=50, threshold=1e-4)
+
+    # Training / Validation runner, the runner tracks the best metric
+    runner = Runner(model, optimizer, transform).to(device)
+
+    # Logging
+    logger = Logger()
+
+    # Checkpoint paths
+    latest_checkpoint = Path('./latest.pt').resolve()
+    best_checkpoint = Path('./best.pt').resolve()
+    persistent_objects = {'optimizer': optimizer, 'scheduler': scheduler, 'model': model, 'runner': runner}
+
+    # Load latest checkpoint if it exists
+    if latest_checkpoint.is_file():
+        load_checkpoint(latest_checkpoint, persistent_objects)
+
+    # Main Training loop
+    initial_epoch = scheduler.last_epoch  # type: ignore
+    print("Training starting from epoch", initial_epoch)
+    if initial_epoch == 0:
+        validate_metrics = runner.eval(validation, initial_epoch, track_metric=track_metric)
+        logger.log_scalars(initial_epoch, validate_metrics=validate_metrics, other={'learning_rate': initial_lr})
+
+    for epoch in range(initial_epoch + 1, max_epochs + 1):
+        start = time.time()
+
+        # Run training and validation
+        train_metrics = runner.train(training, epoch)
+        validate_metrics = runner.eval(validation, epoch, track_metric=track_metric)
+
+        # LR Scheduler update
+        if isinstance(scheduler, ReduceLROnPlateau):  # This scheduler has a different step function
+            scheduler.step(validate_metrics[track_metric])
+        else:
+            scheduler.step()
+
+        # Checkpoint
+        if runner.best_metric_improved_last_run:
+            runner.best_metric_improved_last_run = False
+            save_checkpoint(best_checkpoint, persistent_objects)
+        save_checkpoint(latest_checkpoint, persistent_objects)
+
+        # Logging
+        last_epoch = scheduler.last_epoch  # type: ignore
+        learning_rate = optimizer.param_groups[0]['lr']
+        logger.log_scalars(last_epoch,
+                           train_metrics=train_metrics,
+                           validate_metrics=validate_metrics,
+                           other={'learning_rate': learning_rate,
+                                  'epoch_time_seconds': time.time() - start})
+        # Early stopping
+        if learning_rate < early_stopping_learning_rate:
+            break
