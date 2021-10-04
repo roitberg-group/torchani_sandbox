4de6e69ab08a81d55fd499b2fb8091b04ca77f1a
ipickering <ign.pickering@gmail.com>
2021-10-04 13:29:03 -0400

    improve io tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -30,9 +30,10 @@ class TestUtils(TestCase):
 
     def testToXyz(self):
         with tempfile.NamedTemporaryFile(mode='r+') as f:
-            tensor_to_xyz(f, (expect_species, expect_coords))
-            print(expect_species)
-            species, coordinates, cell = tensor_from_xyz(f, step=1)
+            tensor_to_xyz(f.name, (expect_species, expect_coords), truncate_output_file=True)
+            species, coordinates, cell = tensor_from_xyz(f.name, step=1)
+            self.assertEqual(species, expect_species)
+            self.assertEqual(coordinates, expect_coords)
 
     def testFromXyz(self):
         species, coordinates, cell = tensor_from_xyz(path, step=1)
@@ -40,12 +41,22 @@ class TestUtils(TestCase):
         self.assertEqual(species, expect_species)
 
     def testToLammps(self):
-        species, coordinates, cell = tensor_from_xyz(path, get_cell=False, step=1)
-        tensor_to_lammpstrj(path.with_suffix('.lammpstrj'), (species, coordinates),
-                            cell=torch.eye(3).unsqueeze(0).repeat(len(species), 1, 1), truncate_output_file=True)
+        with tempfile.NamedTemporaryFile(mode='r+') as f:
+            tensor_to_lammpstrj(f.name, (expect_species, expect_coords),
+                                cell=torch.eye(3).unsqueeze(0).repeat(len(expect_species), 1, 1), truncate_output_file=True)
+            species, coordinates, cell, _, _ = tensor_from_lammpstrj(f.name, step=1)
+            self.assertEqual(coordinates, expect_coords)
+            self.assertEqual(species, expect_species)
 
     def testFromLammps(self):
-        species, coordinates, cell, _, _ = tensor_from_lammpstrj(path.with_suffix('.lammpstrj'), get_cell=True, extract_atoms=(0, 2))
+        species, coordinates, cell, _, _ = tensor_from_lammpstrj(path.with_suffix('.lammpstrj'), get_cell=True)
+        self.assertEqual(coordinates, expect_coords)
+        self.assertEqual(species, expect_species)
+
+    def testExtractFromLammps(self):
+        species, coordinates, cell, _, _ = tensor_from_lammpstrj(path.with_suffix('.lammpstrj'), get_cell=True, extract_atoms=(0, 1))
+        self.assertEqual(coordinates, expect_coords.index_select(1, torch.tensor([0, 1]).long()))
+        self.assertEqual(species, expect_species.index_select(1, torch.tensor([0, 1]).long()))
 
 
 if __name__ == '__main__':
