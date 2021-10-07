2f2147f27c970a84bd296d7ccd3f62255d7b6cb4
ipickering <ign.pickering@gmail.com>
2021-10-07 01:26:39 -0400

    fix bug

--- torchani/models.py
+++ torchani/models.py
@@ -71,6 +71,7 @@ from .dispersion import DispersionD3
 from .short_range_basis import EnergySRB
 from .compat import Final
 from . import atomics
+from torchani.utils import map_to_central
 
 
 NN = Union[ANIModel, Ensemble]
@@ -335,6 +336,8 @@ class BuiltinModelPairInteractions(BuiltinModel):
         species_coordinates = self._maybe_convert_species(species_coordinates)
         species, coordinates = species_coordinates
         atom_index12, shift_values, diff_vectors, distances = self.aev_computer.neighborlist(species, coordinates, cell, pbc)
+        if pbc is not None and pbc.any():
+            coordinates = map_to_central(coordinates, cell, pbc)
 
         # energy calculation for potentials with larger cutoff than the aev
         pre_species_energies = (species, torch.zeros(size=(species.shape[0],), device=species.device, dtype=coordinates.dtype))
