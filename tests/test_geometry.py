a47a4985b4ac299e2e83a92ea5136c924ff3a35b
ipickering <ign.pickering@gmail.com>
2021-10-29 13:29:50 -0400

    fix

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -17,7 +17,7 @@ class TestGeometry(TestCase):
         coordinates = torch.tensor([[[0.0, 0.0, 0.0], [0, 1, 1], [1, 0, 1], [1, 1, 0], [0.5, 0.5, 0.5], [0, 0, 0]]], dtype=torch.float)
         species, displaced_coordinates, com = torchani.geometry.displace_to_com_frame((species, coordinates))
         expect_coordinates = coordinates - torch.tensor([[0.5, 0.5, 0.5]]).unsqueeze(1)
-        expect_coordinates[:, (species == -1).nonzero(), :] = 0
+        expect_coordinates[(species == -1), :] = 0
         self.assertEqual(displaced_coordinates, expect_coordinates)
 
     def testCenterToComFrameMany(self):
@@ -26,7 +26,7 @@ class TestGeometry(TestCase):
         coordinates = torch.cat((coordinates, coordinates.clone()), dim=0)
         species, displaced_coordinates, com = torchani.geometry.displace_to_com_frame((species, coordinates))
         expect_coordinates = coordinates - torch.tensor([[0.5, 0.5, 0.5]]).unsqueeze(1)
-        expect_coordinates[:, (species == -1).nonzero(), :] = 0
+        expect_coordinates[(species == -1), :] = 0
         self.assertEqual(displaced_coordinates, expect_coordinates)
 
 if __name__ == '__main__':
