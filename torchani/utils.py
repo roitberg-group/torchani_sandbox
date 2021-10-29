15c0fd33b94aee3cd97d0d02c328bdb1ef16a7f9
richard <yueyericardo@gmail.com>
2021-10-29 14:18:28 -0400

    Compute Dipole

--- torchani/utils.py
+++ torchani/utils.py
@@ -10,6 +10,7 @@ import itertools
 from collections import defaultdict, Counter
 from typing import Tuple, NamedTuple, Optional, Sequence, List, Dict, Union, Mapping
 from torchani.units import sqrt_mhessian2invcm, sqrt_mhessian2milliev, mhessian2fconst
+from .geometry import displace_to_com_frame
 from .nn import SpeciesEnergies
 import numpy as np
 from .compat import tqdm
@@ -81,6 +82,27 @@ def sorted_gsaes(elements: Sequence[str], functional: str, basis_set: str, ):
     """
     gsaes = GSAES[f'{functional.lower()}-{basis_set.lower()}']
     return [gsaes[e] for e in elements]
+
+
+def compute_dipole(species, coordinates, charges, center_of_mass=True):
+    """
+    Compute dipoles in eA
+
+    Arguments:
+        species (torch.Tensor): (M, N), species must be atomic numbers.
+        coordinates (torch.Tensor): (M, N, 3), unit should be Angstrom.
+        charges (torch.Tensor): (M, N), unit should be e.
+        center_of_mass (Bool): When calculating dipole for charged molecule,
+            it is necessary to displace the coordinates to the center-of-mass frame.
+    Returns:
+        dipoles (torch.Tensor): (M, 3)
+    """
+    assert species.shape == charges.shape == coordinates.shape[:-1]
+    charges = charges.unsqueeze(-1)
+    if center_of_mass:
+        _, coordinates = displace_to_com_frame((species, coordinates))
+    dipole = torch.sum(charges * coordinates, dim=1)
+    return dipole
 
 
 def check_openmp_threads():
