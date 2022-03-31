ab1cd046f35da482f38f4d1a257abccbf5c0068b
Kate Huddleston <kdavis2@ufl.edu>
2022-03-31 16:07:56 -0400

    Added Simulation box creation with random molecules inside

--- torchani/aev/flag_overlap.py
+++ torchani/aev/flag_overlap.py
@@ -1,11 +1,13 @@
 import torch
 import torchani
+from torchani.aev.neighbors import BaseNeighborlist
 import numpy as np
 from numpy.random import rand
 
 sim_box = rand(3,3)
 
-xyz = rand(5, 3)
-
-
-
+num_atoms = 100
+random_coordinates = torch.randn(1, num_atoms, 3) * 10
+bn = BaseNeighborlist(5.2)
+bounded_coordinates, cell = bn._compute_bounding_cell(random_coordinates, 1e-2)
+print(cell)
