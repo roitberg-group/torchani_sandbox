5c7ffb8006f4e7a3d7a49aeed83b2020eae2287d
ipickering <ign.pickering@gmail.com>
2022-10-04 14:45:49 -0400

    make periodic table index the default

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -2,30 +2,30 @@ import unittest
 import torchani
 from torchani.testing import TestCase
 
-
+@unittest.skipIf(True, "Very slow")
 class TestLoading(TestCase):
     # check that models loaded from a neurochem source are equal to models
     # loaded directly from a state_dict
 
     def testANI1x(self):
-        model = torchani.models.ANI1x(periodic_table_index=True, use_neurochem_source=False)
-        model_nc = torchani.models.ANI1x(periodic_table_index=True, use_neurochem_source=True)
+        model = torchani.models.ANI1x(use_neurochem_source=False)
+        model_nc = torchani.models.ANI1x(use_neurochem_source=True)
         self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI1xSingle(self):
         for j in range(8):
-            model = torchani.models.ANI1x(periodic_table_index=True, model_index=j, use_neurochem_source=False)
-            model_nc = torchani.models.ANI1x(periodic_table_index=True, model_index=j, use_neurochem_source=True)
+            model = torchani.models.ANI1x(model_index=j, use_neurochem_source=False)
+            model_nc = torchani.models.ANI1x(model_index=j, use_neurochem_source=True)
             self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI2x(self):
-        model = torchani.models.ANI2x(periodic_table_index=True, use_neurochem_source=False)
-        model_nc = torchani.models.ANI2x(periodic_table_index=True, use_neurochem_source=True)
+        model = torchani.models.ANI2x(use_neurochem_source=False)
+        model_nc = torchani.models.ANI2x(use_neurochem_source=True)
         self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI1ccx(self):
-        model = torchani.models.ANI1ccx(periodic_table_index=True, use_neurochem_source=False)
-        model_nc = torchani.models.ANI1ccx(periodic_table_index=True, use_neurochem_source=True)
+        model = torchani.models.ANI1ccx(use_neurochem_source=False)
+        model_nc = torchani.models.ANI1ccx(use_neurochem_source=True)
         self.assertEqual(model_nc.state_dict(), model.state_dict())
 
 
