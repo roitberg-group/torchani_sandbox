40a2f6456324a2ea346ecebf5fa900a5862b230b
Nick <nterrel@ufl.edu>
2023-03-28 13:52:54 -0400

    added two data classes (AtomicQBCs, ForceQBCs) and two functions (atomic_qbcs, force_qbcs)

--- torchani/models.py
+++ torchani/models.py
@@ -87,19 +87,16 @@ class SpeciesEnergiesQBC(NamedTuple):
     energies: Tensor
     qbcs: Tensor
 
-
 class AtomicQBCs(NamedTuple):
     species: Tensor
     energies: Tensor
     ae_stdev: Tensor
-
 
 class ForceQBCs(NamedTuple):
     species: Tensor
     energies: Tensor
     mean_force: Tensor
     stdev_force: Tensor
-
 
 class BuiltinModel(Module):
     r"""Private template for the builtin ANI models """
@@ -310,13 +307,13 @@ class BuiltinModel(Module):
     def atomic_qbcs(self, species_coordinates: Tuple[Tensor, Tensor],
                     cell: Optional[Tensor] = None,
                     pbc: Optional[Tensor] = None,
-                    average: bool = False,
+                    average: bool = False, 
                     with_SAEs: bool = False,
                     unbiased: bool = True) -> AtomicQBCs:
-        """
-        Largely does the same thing as the atomic_energies function, but with a different set of default inputs.
-        Returns standard deviation in atomic energy predictions across the ensemble.
-        """
+        '''
+        Largely does the same thing as the atomic_energies function, but with a different set of default inputs. 
+        Returns standard deviation in atomic energy predictions across the ensemble. 
+        '''
         assert isinstance(self.neural_networks, Ensemble), "Your model doesn't have an ensemble of networks"
         species_coordinates = self._maybe_convert_species(species_coordinates)
         species_aevs = self.aev_computer(species_coordinates, cell=cell, pbc=pbc)
@@ -333,27 +330,27 @@ class BuiltinModel(Module):
         # Want to return with GSAEs, but that can wait
         if with_SAEs:
             atomic_energies += self.energy_shifter._atomic_saes(species_coordinates[0])
-            # atomic_energies += self.energy_shifter.with_gsaes(species_coordinates[0], 'wb97x', '631gd')
+            #atomic_energies += self.energy_shifter.with_gsaes(species_coordinates[0], 'wb97x', '631gd')
 
         return AtomicQBCs(species_coordinates[0], atomic_energies, ae_stdev)
 
     def force_qbcs(self, species_coordinates: Tuple[Tensor, Tensor],
                    cell: Optional[Tensor] = None,
                    pbc: Optional[Tensor] = None,
-                   average: bool = False,
-                   unbiased: bool = True) -> ForceQBCs:
+                   average: bool = False) -> ForceQBCs:
         assert isinstance(self.neural_networks, Ensemble), "Your model doesn't have an ensemble of networks"
-        species_coordinates[1].requires_grad = True
+        species_coordinates[1].requires_grad=True
+        #species_coordinates = self._maybe_convert_species(species_coordinates)     # This is only needed if periodic_table_index=False
         members_energies = self.members_energies(species_coordinates, cell, pbc).energies
-        forces_list = []
+        forces = []
 
         for energy in members_energies:
-            derivative = torch.autograd.grad(energy, species_coordinates[1], retain_graph=True)[0]
+            derivative = torch.autograd.grad(energy,species_coordinates[1],retain_graph=True)[0]
             force = -derivative
-            forces_list.append(force)
-        forces = torch.cat(forces_list, dim=0)
+            forces.append(force)
+        forces = torch.cat(forces, dim=0)
         mean_force = forces.mean(0)
-        stdev_force = forces.std(0, unbiased=unbiased)
+        stdev_force = forces.std(0)
 
         return ForceQBCs(species_coordinates[0], members_energies, mean_force, stdev_force)
 
