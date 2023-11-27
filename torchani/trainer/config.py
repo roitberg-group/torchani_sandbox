907e08bb752caf8c7ec4976572d6ef630cf2a884
Kate Huddleston <kdavis2@ufl.edu>
2023-11-27 14:34:14 -0500

    Updated config to return dictionary, model_config. It now specifys constants and elements, and has clear assert statements.

--- torchani/trainer/config.py
+++ torchani/trainer/config.py
@@ -1,8 +1,8 @@
 from configparser import ConfigParser
 
+
 class TrainerConfig:
 
-    
     def __init__(self, config_file, device):
         self.config = self.load_config(config_file)
         self.device = device
@@ -25,11 +25,22 @@ class TrainerConfig:
         for key, value in tv.items():
             self._convert(key, value, model_config)
 
-        return model_config 
+        assert not (model_config['netlike1x'] and model_config['netlike2x']), "In configuration file, netlike1x and netlike2x cannot both be True"
+
+        if model_config['netlike1x']:
+            model_config['constants']  = '../resources/ani-1x_8x/rHCNO-5.2R_16-3.5A_a4-8.params'
+            model_config['elements'] = ['H', 'C', 'N', 'O']
+        elif model_config['netlike2x']:
+            model_config['constants'] = '../resources/ani-2x_8x/rHCNOSFCl-5.1R_16-3.5A_a8-4.params'
+            model_config['elements'] = ['H', 'C', 'N', 'O', 'S', 'F', 'Cl']
+        else: 
+            assert 'constants' in model_config and model_config['constants'] is not None, "constants not specified in configuration file."
+            assert 'elements' in model_config and model_config['elements'] is not None, "elements not specified in configuration file."
+        return model_config
 
     def _convert(self, key, value, model_config):
         try:
             model_config[key] = eval(value)
 
-        except(NameError, SyntaxError):
+        except (NameError, SyntaxError):
             model_config[key] = value
