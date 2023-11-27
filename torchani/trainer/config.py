ecef904e39a0113e8c6042d13555591af13a2c0c
Kate Huddleston <kdavis2@ufl.edu>
2023-11-27 13:56:47 -0500

    Fixing flake8 and removing model_build.py

--- torchani/trainer/config.py
+++ torchani/trainer/config.py
@@ -1,12 +1,13 @@
 from configparser import ConfigParser
-from pathlib import Path
 
 class TrainerConfig:
+
+    
     def __init__(self, config_file, device):
         self.config = self.load_config(config_file)
         self.device = device
         self.model_config = self.parse_config()
-    
+
     def load_config(self, config_file):
         config = ConfigParser(allow_no_value=True, inline_comment_prefixes="#")
         config.read(config_file)
@@ -15,16 +16,16 @@ class TrainerConfig:
     def parse_config(self):
         gv = self.config['Global']
         tv = self.config['Trainer']
-        
+
         model_config = {}
 
         for key, value in gv.items():
             self._convert(key, value, model_config)
 
-        for key,value in tv.items():
+        for key, value in tv.items():
             self._convert(key, value, model_config)
-        
-        return model_config
+
+        return model_config 
 
     def _convert(self, key, value, model_config):
         try:
