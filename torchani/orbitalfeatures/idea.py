46be640cf0456f1d95c04b3f2b1c9a4eb1b59587
ipickering <ign.pickering@gmail.com>
2024-04-25 20:08:07 -0400

    Simplify ideas

--- torchani/orbitalfeatures/idea.py
+++ torchani/orbitalfeatures/idea.py
@@ -2,20 +2,21 @@ import typing as tp
 import torch
 from torch import Tensor
 
-#from torchani.aev import AEVComputer
-#from torchani.aev.aev_terms import StandardAngular, StandardRadial
-#from torchani.orbitalfeatures.orb_utils import AtomicNumsToInts
+# from torchani.aev import AEVComputer
+# from torchani.aev.aev_terms import StandardAngular, StandardRadial
+# from torchani.orbitalfeatures.orb_utils import AtomicNumsToInts
 
+# SimpleOrbitalAEVCalculator
 class ExCorrAEVComputerVariation(torch.nn.Module):
     def __init__(self) -> None:
-
         super().__init__()
 
     def forward(
         self,
         species: Tensor,
         coefficients: Tensor,
-    ) -> tp.Tuple[Tensor, Tensor, Tensor, Tensor]:
+    ) -> Tensor:
+        # ) -> tp.Tuple[Tensor, Tensor, Tensor, Tensor]:
         # We first need to reshape the coefficients for their manipulation
         # The s-type coefficients are processed with a custom radial AEV computer
         # The p and d-type coefficientes form an orbital matrix that basically
@@ -25,16 +26,20 @@ class ExCorrAEVComputerVariation(torch.nn.Module):
 
         # In order to use the AEV computer function from the geometric AEVs, we need
         # to generate arrays that can serve as input for such function
-        neighbor_idxs, distances = self._get_aev_inputs(orbital_matrix)
+        # neighbor_idxs, distances = self._get_aev_inputs(orbital_matrix)
 
-        return s_coeffs, orbital_matrix, neighbor_idxs, distances
+        distances = torch.linalg.norm(orbital_matrix, dim=-1)
+        simple_orbital_aevs = torch.cat((s_coeffs, distances), dim=-1)
+        return simple_orbital_aevs  # shape (nconf, natoms, 21)
+
+        # return s_coeffs, orbital_matrix, neighbor_idxs, distances
 
     def _reshape_coefficients(
         self,
         coefficients: Tensor,
-    ) -> tp.Tuple[Tensor, Tensor]: 
+    ) -> tp.Tuple[Tensor, Tensor]:
         """ Output: A tuple containing 2 tensors: one with the s-type coefficients,
-        and another with the p and d-type coefficients in the form of a matrix. 
+        and another with the p and d-type coefficients in the form of a matrix.
 
         The obtained orbital matrix will look like:
 
@@ -52,9 +57,9 @@ class ExCorrAEVComputerVariation(torch.nn.Module):
         """
         nconformers, natoms, _ = coefficients.shape
 
-        s_coeffs = coefficients[:,:,:9]   # Shape: (nconformers, natoms, 9)
-        p_coeffs = coefficients[:,:,9:21] # Shape: (nconformers, natoms, 12)
-        d_coeffs = coefficients[:,:,21:]  # Shape: (nconformers, natoms, 24)
+        s_coeffs = coefficients[:, :, :9]    # Shape: (nconformers, natoms, 9)
+        p_coeffs = coefficients[:, :, 9:21]  # Shape: (nconformers, natoms, 12)
+        d_coeffs = coefficients[:, :, 21:]   # Shape: (nconformers, natoms, 24)
 
         # Reshape p_coeffs to make it easier to handle individual components
         p_coeffs_reshaped = p_coeffs.view(nconformers, natoms, 4, 3)  # Shape: (nconformers, natoms, 4, 3)
@@ -63,9 +68,9 @@ class ExCorrAEVComputerVariation(torch.nn.Module):
         # Correcting the reordering of d_coeffs
         # Transformation [Dxx, Dxy, Dyy, Dzx, Dzy, Dzz] -> [Dxx, Dyy, Dzz, Dzy, Dzx, Dxy]
         # which maps to indices [0, 2, 5, 4, 3, 1] respectively
-        d_coeffs_reshaped = d_coeffs.view(nconformers, natoms, 4, 6)  # Shape: (nconformers, natoms, 4, 6) 
+        d_coeffs_reshaped = d_coeffs.view(nconformers, natoms, 4, 6)  # Shape: (nconformers, natoms, 4, 6)
         d_coeffs_reshaped_reordered = d_coeffs_reshaped[:, :, :, [0, 2, 5, 4, 3, 1]]
-        d_coeffs_reshaped_reordered = d_coeffs_reshaped_reordered.view(nconformers, natoms, 8, 3)  # Shape: (nconformers, natoms, 8, 3)        
+        d_coeffs_reshaped_reordered = d_coeffs_reshaped_reordered.view(nconformers, natoms, 8, 3)  # Shape: (nconformers, natoms, 8, 3)
 
         # Splitting into two groups: diagonal [Dxx, Dyy, Dzz] and off-diagonal [Dzy, Dzx, Dxy]
         d_diagonal = d_coeffs_reshaped_reordered[:, :, :3]
@@ -81,13 +86,13 @@ class ExCorrAEVComputerVariation(torch.nn.Module):
         orbital_matrix: Tensor,
     ) -> tp.Tuple[Tensor, Tensor, Tensor]:
         """ Output: A tuple containing 3 tensors resembling the neighbor_idxs,
-        distances and diff_vectors tensors from the geometric AEVs. 
-        
+        distances and diff_vectors tensors from the geometric AEVs.
+
         To construct the neighbor_idxs tensor, a set of 12 (4 p + 8 d) fake atoms
         is "attached" to each conformer. The coordinates of these fake atoms are
         given in the "coefficients" space, and are those of the corresponding AOVs.
-        
-        This way, if a given confomer has natoms the indexes 0:natoms belong to 
+
+        This way, if a given confomer has natoms the indexes 0:natoms belong to
         actual atoms, while the indexes natoms:natoms+12 represent the fake atoms.
 
         For example, let's consider the case in which we only have two corformers:
@@ -96,34 +101,46 @@ class ExCorrAEVComputerVariation(torch.nn.Module):
 
         [ 2  2  2  ... 2   0  0  0 ... 0   1  1  1 ... 1  ]
         [ 3  4  5  ... 14  3  4  5 ... 14  3  4  5 ... 14 ]
-          
-        Here the first row correspond to actual atoms indexes, while the second one 
-        corresponds to fake atoms. Note that natoms is 3, that is the size of the 
+
+        Here the first row correspond to actual atoms indexes, while the second one
+        corresponds to fake atoms. Note that natoms is 3, that is the size of the
         larger molecule.
 
-        The distances tensor simply involves evaluating the module of the AOVs. 
-        In other words, the only distances we are worried about are the distances 
+        The distances tensor simply involves evaluating the module of the AOVs.
+        In other words, the only distances we are worried about are the distances
         between the actual atom and a 12 "fake" atoms in the coordinates of the AOVs.
 
         In the example from above, the distances tensor would look like:
         [ d2-3  d2-4  d2-5  ... d2-14  d0-3  d0-4  d0-5 ... d0-14  d1-3  d1-4  d1-5 ... d1-14 ]
 
-        The diff_vectors tesor follows the same idea. 
-          
+        The diff_vectors tesor follows the same idea.
         """
-        nconformers, natoms, _ , _ = orbital_matrix.shape
+        # orbital_matrix = orbital_matrix.view(-1, 12, 3)
+        # First dimension is natoms * nconformers * 12
+        orbital_matrix = orbital_matrix.view(-1, 12, 3)
+        distances = torch.linalg.norm(orbital_matrix, dim=-1)
+        ntotal = orbital_matrix.shape[0] * orbital_matrix.shape[1]
+        # nconformers, natoms, _, _ = orbital_matrix.shape
 
-        # Create a new tensor of zeros with the desired shape (nconformers, natoms, 2, 12)
-        neighbor_idxs = torch.zeros((nconformers, natoms, 2, 12), dtype=torch.int)
-        # Assigning the sequence to each position in the third dimension slice 2
-        neighbor_idxs[:, :, 1, :] = torch.arange(natoms, natoms+12, dtype=torch.int)
+        # TODO: This may not be needed, it is kind of strange that we need to do it
+        # ntotal = nconformers * natoms
+        top = torch.repeat_interleave(
+            torch.arange(ntotal, dtype=torch.long, device=orbital_matrix.device),
+            12,
+        )
+        fake_atom_idxs = torch.arange(ntotal, ntotal + 12, dtype=torch.long, device=orbital_matrix.device)
+        bottom = torch.tile(fake_atom_idxs, (ntotal,))
+        neighbor_idxs = torch.cat((top.unsqueeze(0), bottom.unsqueeze(0)), dim=0)
+
+        # neighbor_idxs[:, :, 0, :] = torch.arange(0, natoms, dtype=torch.long)
+        # neighbor_idxs[:, :, 1, :] =
         # Calculate modules of the AOVs
-        distances = torch.sqrt((orbital_matrix ** 2).sum(dim=-1))       
+        # distances = torch.sqrt((orbital_matrix ** 2).sum(dim=-1))
+        # distances = torch.linalg.norm(orbital_matrix)
         # Permute the tensor to rearrange the dimensions to (2, nconformers, natoms, 12)
-        distances = distances.permute(2, 0, 1, 3) 
-        # Flatten all dimensions except the first into one dimension
-        distances = distances.reshape(2, -1)  # Now shape (2, npairs), where npairs = 12*natoms*nconformers
+        # distances = distances.permute(2, 0, 1, 3)
+        # # Flatten all dimensions except the first into one dimension
+        # distances = distances.reshape(2, -1)  # Now shape (2, npairs), where npairs = 12*natoms*nconformers
 
-        # For now it only works 
+        # For now it only works
         return neighbor_idxs, distances
-
