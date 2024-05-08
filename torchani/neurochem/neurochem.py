91ce08177d01d916dd9afcc8558181b5f71d562e
ipickering <ign.pickering@gmail.com>
2024-05-08 17:48:58 -0400

    Remove Constants class

--- torchani/neurochem/neurochem.py
+++ torchani/neurochem/neurochem.py
@@ -8,6 +8,7 @@ import os
 from collections import OrderedDict
 
 import torch
+from torch import Tensor
 import lark
 
 from torchani.nn import ANIModel, Ensemble
@@ -20,37 +21,6 @@ class NeurochemParseError(RuntimeError):
 
 
 class Constants(collections.abc.Mapping):
-    """NeuroChem constants. Objects of this class can be used as arguments
-    to :class:`torchani.AEVComputer`, like ``torchani.AEVComputer(**consts)``.
-
-    Attributes:
-        species_to_tensor (:class:`ChemicalSymbolsToInts`): call to convert
-            string chemical symbols to 1d long tensor.
-    """
-
-    def __init__(self, filename):
-        self.filename = filename
-        with open(filename) as f:
-            for i in f:
-                try:
-                    line = [x.strip() for x in i.split('=')]
-                    name = line[0]
-                    value = line[1]
-                    if name == 'Rcr' or name == 'Rca':
-                        setattr(self, name, float(value))
-                    elif name in ['EtaR', 'ShfR', 'Zeta',
-                                  'ShfZ', 'EtaA', 'ShfA']:
-                        float_values = [float(x.strip()) for x in value.replace(
-                            '[', '').replace(']', '').split(',')]
-                        setattr(self, name, torch.tensor(float_values))
-                    elif name == 'Atyp':
-                        str_values = [x.strip() for x in value.replace(
-                            '[', '').replace(']', '').split(',')]
-                        self.species = str_values
-                except Exception:
-                    raise NeurochemParseError(f'Unable to parse const file {filename}') from None
-        self.num_species = len(self.species)
-        self.species_to_tensor = ChemicalSymbolsToInts(self.species)
 
     def __iter__(self):
         yield 'Rcr'
@@ -68,6 +38,29 @@ class Constants(collections.abc.Mapping):
 
     def __getitem__(self, item):
         return getattr(self, item)
+
+
+def load_constants(filename: tp.Union[Path, str]) -> tp.Tuple[tp.Dict[str, Tensor], tp.Dict[str, float], tp.Tuple[str, ...]]:
+    aev_constants: tp.Dict[str, Tensor] = {}
+    aev_cutoffs: tp.Dict[str, float] = {}
+    with open(filename) as f:
+        for i in f:
+            try:
+                line = [x.strip() for x in i.split('=')]
+                name = line[0]
+                value = line[1]
+                if name in ['Rcr', 'Rca']:
+                    aev_cutoffs[name] = float(value)
+                elif name in ['EtaR', 'ShfR', 'Zeta', 'ShfZ', 'EtaA', 'ShfA']:
+                    float_values = [float(x.strip()) for x in value.replace(
+                        '[', '').replace(']', '').split(',')]
+                    aev_constants[name] = torch.tensor(float_values)
+                elif name == 'Atyp':
+                    species = tuple(x.strip() for x in value.replace(
+                        '[', '').replace(']', '').split(','))
+            except Exception:
+                raise NeurochemParseError(f'Unable to parse const file {filename}') from None
+    return aev_constants, aev_cutoffs, species
 
 
 def load_sae(filename: tp.Union[Path, str], return_dict: bool = False):
