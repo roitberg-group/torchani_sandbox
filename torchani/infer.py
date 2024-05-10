8c65f2f8d02e1983133fcd91632c56fc6038eaf6
ipickering <ign.pickering@gmail.com>
2024-05-09 20:32:41 -0400

    Fix bug

--- torchani/infer.py
+++ torchani/infer.py
@@ -137,7 +137,7 @@ class BmmAtomicNetwork(torch.nn.Module):
         for layer_idx, layer in enumerate(networks[0]):
             if type(layer) is torch.nn.Linear:
                 layers.append(BmmLinear([net[layer_idx] for net in networks]))
-            elif type(layer) is (torch.nn.CELU, torch.nn.GELU):
+            elif type(layer) in (torch.nn.CELU, torch.nn.GELU):
                 layers.append(layer)
             else:
                 raise ValueError("Only GELU/CELU act. fn and Linear layers supported")
