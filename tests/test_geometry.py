3f5d1f605c4351c7bea8c7316a60d4070940d530
ipickering <ign.pickering@gmail.com>
2024-05-12 22:11:21 -0400

    Modernize geometry test (fails due to non-jittable get_atomic_masses)

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,26 +1,38 @@
 import unittest
 import torch
-import torchani
-from torchani.testing import TestCase
+from torchani.testing import ANITest, expand
+from torchani.geometry import displace_to_com_frame
+from torchani.units import bohr2angstrom
 
 
-class TestGeometry(TestCase):
+@expand()
+class TestDisplaceToCom(ANITest):
+    def setUp(self) -> None:
+        if self.jit:
+            self.fn = torch.jit.script(displace_to_com_frame)
+        else:
+            self.fn = displace_to_com_frame
+
     def testCenterToComFrame(self):
-        species = torch.tensor([[1, 1, 1, 1, 6]], dtype=torch.long)
+        species = torch.tensor([[1, 1, 1, 1, 6]], dtype=torch.long, device=self.device)
         coordinates = torch.tensor(
             [[[0.0, 0.0, 0.0], [0, 1, 1], [1, 0, 1], [1, 1, 0], [0.5, 0.5, 0.5]]],
             dtype=torch.float,
+            device=self.device,
         )
-        species, displaced_coordinates = torchani.geometry.displace_to_com_frame(
-            (species, coordinates)
-        )
+        species, displaced_coordinates = self.fn((species, coordinates))
         self.assertEqual(
             displaced_coordinates,
-            coordinates - torch.tensor([[0.5, 0.5, 0.5]]).unsqueeze(1),
+            coordinates
+            - torch.tensor(
+                [[0.5, 0.5, 0.5]], device=self.device, dtype=torch.float
+            ).unsqueeze(1),
         )
 
     def testCenterToComFrameDummy(self):
-        species = torch.tensor([[1, 1, 1, 1, 6, -1]], dtype=torch.long)
+        species = torch.tensor(
+            [[1, 1, 1, 1, 6, -1]], dtype=torch.long, device=self.device
+        )
         coordinates = torch.tensor(
             [
                 [
@@ -34,16 +46,18 @@ class TestGeometry(TestCase):
             ],
             dtype=torch.float,
         )
-        species, displaced_coordinates = torchani.geometry.displace_to_com_frame(
-            (species, coordinates)
-        )
-        expect_coordinates = coordinates - torch.tensor([[0.5, 0.5, 0.5]]).unsqueeze(1)
+        species, displaced_coordinates = self.fn((species, coordinates))
+        expect_coordinates = coordinates - torch.tensor(
+            [[0.5, 0.5, 0.5]], device=self.device, dtype=torch.float
+        ).unsqueeze(1)
         expect_coordinates[(species == -1)] = 0
         self.assertEqual(displaced_coordinates, expect_coordinates)
 
     def testCenterToComFrameMany(self):
         species = torch.tensor(
-            [[1, 1, 1, 1, 6, -1], [6, 6, 6, 6, 8, -1]], dtype=torch.long
+            [[1, 1, 1, 1, 6, -1], [6, 6, 6, 6, 8, -1]],
+            dtype=torch.long,
+            device=self.device,
         )
         coordinates = torch.tensor(
             [
@@ -57,12 +71,13 @@ class TestGeometry(TestCase):
                 ]
             ],
             dtype=torch.float,
+            device=self.device,
         )
         coordinates = torch.cat((coordinates, coordinates.clone()), dim=0)
-        species, displaced_coordinates = torchani.geometry.displace_to_com_frame(
-            (species, coordinates)
-        )
-        expect_coordinates = coordinates - torch.tensor([[0.5, 0.5, 0.5]]).unsqueeze(1)
+        species, displaced_coordinates = self.fn((species, coordinates))
+        expect_coordinates = coordinates - torch.tensor(
+            [[0.5, 0.5, 0.5]], device=self.device, dtype=torch.float
+        ).unsqueeze(1)
         expect_coordinates[(species == -1)] = 0
         self.assertEqual(displaced_coordinates, expect_coordinates)
 
@@ -78,13 +93,14 @@ class TestGeometry(TestCase):
                 ]
             ],
             dtype=torch.float,
+            device=self.device,
         )
-        species, displaced_coordinates = torchani.geometry.displace_to_com_frame(
-            (species, coordinates)
-        )
+        species, displaced_coordinates = self.fn((species, coordinates))
         # com = coordinates + displaced_coordinates
-        expect_com = torch.tensor([[0.038116, 0.098033, 0.000000]])
-        expect_com = torchani.units.bohr2angstrom(expect_com)
+        expect_com = torch.tensor(
+            [[0.038116, 0.098033, 0.000000]], device=self.device, dtype=torch.float
+        )
+        expect_com = bohr2angstrom(expect_com)
         self.assertEqual(displaced_coordinates, coordinates - expect_com)
 
 
