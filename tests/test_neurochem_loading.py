124f64df52501838836d9bb43e75eddb28009db4
ipickering <ign.pickering@gmail.com>
2024-05-17 13:34:27 -0400

    Improve neurochem loading to fix example file

--- tests/test_neurochem_loading.py
+++ tests/test_neurochem_loading.py
@@ -1,7 +1,7 @@
 import unittest
 
 from torchani.testing import TestCase
-from torchani.neurochem import load_builtin
+from torchani.neurochem import load_builtin_from_name
 from torchani.models import ANI1x, ANI2x, ANI1ccx
 
 
@@ -11,29 +11,29 @@ class TestLoading(TestCase):
 
     def testANI1x(self):
         model = ANI1x()
-        model_nc = load_builtin("ani1x")
+        model_nc = load_builtin_from_name("ani1x")
         self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI1xSingle(self):
         for j in range(8):
             model = ANI1x(model_index=j)
-            model_nc = load_builtin("ani1x", model_index=j)
+            model_nc = load_builtin_from_name("ani1x", model_index=j)
             self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI2x(self):
         model = ANI2x()
-        model_nc = load_builtin("ani2x")
+        model_nc = load_builtin_from_name("ani2x")
         self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI2xSingle(self):
         for j in range(8):
             model = ANI2x(model_index=j)
-            model_nc = load_builtin("ani2x", model_index=j)
+            model_nc = load_builtin_from_name("ani2x", model_index=j)
             self.assertEqual(model_nc.state_dict(), model.state_dict())
 
     def testANI1ccx(self):
         model = ANI1ccx()
-        model_nc = load_builtin("ani1ccx")
+        model_nc = load_builtin_from_name("ani1ccx")
         self.assertEqual(model_nc.state_dict(), model.state_dict())
 
 
