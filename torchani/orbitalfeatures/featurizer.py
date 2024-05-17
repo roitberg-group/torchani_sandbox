b89faa1c004481c4d2990d4d5a7481725b98f34b
Jonathan Semelak <jota.smk@gmail.com>
2024-05-17 18:51:33 -0300

    include angular option in featurizer

--- torchani/orbitalfeatures/featurizer.py
+++ torchani/orbitalfeatures/featurizer.py
@@ -26,11 +26,15 @@ class ExCorrAEVComputer(AEVComputer):
         neighborlist='full_pairwise',
         radial_terms='standard',
         angular_terms='standard',
+        use_simple_orbital_aev: bool = True,
+        use_angular_info_in_simple_orbital_aev: bool = False,
+        basis_functions='spd',
         use_geometric_aev: bool = True,
     ) -> None:
         # forbid cuaev for now
         assert not use_cuda_extension
         assert not use_cuaev_interface
+        assert (basis_functions == 'spd' or basis_functions == 'sp' or basis_functions == 's')
         super().__init__(
             Rcr,
             Rca,
@@ -48,12 +52,31 @@ class ExCorrAEVComputer(AEVComputer):
             radial_terms,
             angular_terms,
         )
+        self.use_simple_orbital_aev = use_simple_orbital_aev
+        self.use_angular_info_in_simple_orbital_aev = use_angular_info_in_simple_orbital_aev
+        self.basis_functions = basis_functions
         self.use_geometric_aev = use_geometric_aev
-        self.simple_orbital_aev_computer = SimpleOrbitalAEVComputer()
-        if use_geometric_aev:
-            self.aev_length = self.radial_length + self.angular_length + 21
+        if use_simple_orbital_aev:
+            if basis_functions == 'spd':
+                orbital_aev_length = 21
+            elif basis_functions == 'sp':
+                orbital_aev_length = 13            
+            elif basis_functions == 's':
+                orbital_aev_length = 9
+            if use_angular_info_in_simple_orbital_aev:
+                #Only p and d AOVs have angular info associated
+                #The number of s+d AOVs is simple_orbital_aev_length-9 (because we need to substract the 9 s AOVs)
+                #If we calculate the number of angles as N(N-1)/2 (with N the number of s+d AOVs), we have:
+                orbital_aev_length = orbital_aev_length + ((orbital_aev_length-9)*(orbital_aev_length-10)/2)
+            self.orbital_aev_computer = SimpleOrbitalAEVComputer()   
         else:
-            self.aev_length = 21
+            #To do -> Include an AEV-like expansion for the AOVs
+            pass
+
+        if use_geometric_aev:
+            self.aev_length = self.radial_length + self.angular_length + orbital_aev_length
+        else:
+            self.aev_length = orbital_aev_length
 
     def forward(  # type: ignore
         self,
@@ -76,7 +99,12 @@ class ExCorrAEVComputer(AEVComputer):
         # WARNING: The coordinates that are input into the neighborlist are **not** assumed to be
         # mapped into the central cell for pbc calculations,
         # and **in general are not**
-        aev = self.simple_orbital_aev_computer(coefficients)
+
+        if self.use_simple_orbital_aev:
+            aev = self.orbital_aev_computer(coefficients,self.use_angular_info_in_simple_orbital_aev)
+        else:
+            #To do -> Same but for an AEV-like expansion of the AOVs
+            aev = torch.tensor([])  # Placeholder until implemented
 
         if self.use_geometric_aev:
             neighbor_data = self.neighborlist(species, coordinates, self.radial_terms.cutoff, cell, pbc)
