b6197c4d26cbd9f746e70c91fc82e0ab21acb842
ipickering <ign.pickering@gmail.com>
2024-05-23 14:28:26 -0400

    Improve cell reading logic

--- torchani/io.py
+++ torchani/io.py
@@ -8,6 +8,10 @@ from torch import Tensor
 from torchani.utils import ATOMIC_NUMBERS, pad_atomic_properties
 
 
+class TorchaniIOError(IOError):
+    pass
+
+
 def read_xyz(
     path: tp.Union[str, Path],
     dtype: torch.dtype = torch.float,
@@ -19,12 +23,15 @@ def read_xyz(
     A) and (C, A, 3) respectively, where C is the number of conformations, A
     the maximum number of atoms (conformations with less atoms are padded with
     species=-1 and coordinates=0.0).
+
+    Cell is read from the first conformation.
     """
     path = Path(path).resolve()
     cell: tp.Optional[Tensor] = None
     properties: tp.List[tp.Dict[str, Tensor]] = []
     with open(path, mode="rt", encoding="utf-8") as f:
         lines = iter(f)
+        conformation_num = 0
         while True:
             species = []
             coordinates = []
@@ -34,17 +41,27 @@ def read_xyz(
                 break
             comment = next(lines)
             if "lattice" in comment.lower():
+                if (cell is None) and (conformation_num != 0):
+                    raise TorchaniIOError(
+                        "If cell is present it should be in the first conformation"
+                    )
                 parts = shlex.split(comment)
                 for part in parts:
                     key, value = part.split("=")
                     if key.lower() == "lattice":
                         # cell order is x0 y0 z0 x1 y1 z1 x2 y2 z2 for the
                         # 3 unit vectors
-                        cell = torch.tensor(
+                        conformation_cell = torch.tensor(
                             [float(s) for s in value.split()],
                             dtype=dtype,
                             device=device,
                         ).view(3, 3)
+                        if cell is None:
+                            cell = conformation_cell
+                        elif not (cell == conformation_cell).all():
+                            raise TorchaniIOError(
+                                "Found two conformations with non-matching cells"
+                            )
             for _ in range(num):
                 line = next(lines)
                 s, x, y, z = line.split()
@@ -53,6 +70,7 @@ def read_xyz(
                 else:
                     species.append(int(s))
                 coordinates.append([float(x), float(y), float(z)])
+            conformation_num += 1
             properties.append(
                 {
                     "coordinates": torch.tensor(
