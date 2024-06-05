0c83e7a89bf0b5e492aa38744121ff386f49cdc7
ipickering <ign.pickering@gmail.com>
2024-06-05 17:07:47 -0400

    Simplify recipe more, use pip install instead of cmake

--- recipe/build.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_HOME=/usr/local/cuda
-python -m pip install --no-deps --no-build-isolation -v .
-cd ./torchani/csrc/ || exit 1
-mkdir ./build
-cd ./build || exit 1
-cmake -DTORCH_CUDA_ARCH_LIST='6.0;6.1;7.0;7.5;8.0;8.6' .. && cmake --build . && cmake --install .
