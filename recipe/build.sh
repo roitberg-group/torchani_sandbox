67c6962e4c7732b4c7dffac7f2f9ecc4b588fae1
ipickering <ign.pickering@gmail.com>
2024-06-05 17:01:15 -0400

    Fix build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,5 @@ export CUDA_HOME=/usr/local/cuda
 python -m pip install --no-deps --no-build-isolation -v .
 cd ./torchani/csrc/ || exit 1
 mkdir ./build
-cmake -DTORCH_CUDA_ARCH_LIST='6.0;6.1;7.0;7.5;8.0;8.6' -S. -B./build/
-cmake --build ./build
-cmake --install ./build
+cd ./build || exit 1
+cmake -DTORCH_CUDA_ARCH_LIST='6.0;6.1;7.0;7.5;8.0;8.6' .. && cmake --build . && cmake --install .
