48b227574a1a65afeb5ceca2d1c1ae7f60616569
ipickering <ign.pickering@gmail.com>
2024-06-06 12:25:12 -0400

    Add script to upload conda pkgs

---
+++ ci/upload_conda_pkg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script is used to upload conda packages to prevent github from leaking
+# secrets (DOCKER_PVTKEY and MORIA_USERNAME) in the log of the workflow files.
+# All secrets should be passed as envvars
+
+echo "${DOCKER_PVTKEY}"  > ~/.ssh/id_rsa.pub && chmod 400 ~/.ssh/id_rsa
+rsync -av --delete ./conda-pkgs/ "${MORIA_USERNAME}@moria.chem.ufl.edu:/conda-pkgs/"
