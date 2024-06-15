d14909376e3b2102d2e3a8577f50a7eecedff7c6
ipickering <ign.pickering@gmail.com>
2024-06-15 11:25:05 -0400

    Fix paths

--- torchani/paths.py
+++ torchani/paths.py
@@ -4,12 +4,12 @@ Default location for various torchani resources
 from pathlib import Path
 
 LOCAL_DIR = (Path.home() / ".local") / "torchani"
-STATE_DICTS_DIR = LOCAL_DIR / "StateDicts"
-DATASETS_DIR = LOCAL_DIR / "Datasets"
-NEUROCHEM_DIR = LOCAL_DIR / "Neurochem"
+STATE_DICTS = LOCAL_DIR / "StateDicts"
+DATASETS = LOCAL_DIR / "Datasets"
+NEUROCHEM = LOCAL_DIR / "Neurochem"
 
 LOCAL_DIR.mkdir(exist_ok=True, parents=True)
-STATE_DICTS_DIR.mkdir(exist_ok=True, parents=True)
-DATASETS_DIR.mkdir(exist_ok=True, parents=True)
-NEUROCHEM_DIR.mkdir(exist_ok=True, parents=True)
-RESOURCES_DIR = Path(__file__).resolve().parent / "resources"
+STATE_DICTS.mkdir(exist_ok=True, parents=True)
+DATASETS.mkdir(exist_ok=True, parents=True)
+NEUROCHEM.mkdir(exist_ok=True, parents=True)
+RESOURCES = Path(__file__).resolve().parent / "resources"
