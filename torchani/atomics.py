ae27007de9abc36d6d7c63f1d352265ea76204c7
ipickering <ign.pickering@gmail.com>
2024-09-26 01:42:27 -0400

    Mypy

--- torchani/atomics.py
+++ torchani/atomics.py
@@ -15,7 +15,7 @@ class AtomicContainer(torch.nn.Module):
     num_networks: int
     num_species: int
 
-    def __init__(self) -> None:
+    def __init__(self, *args: tp.Any, **kwargs: tp.Any) -> None:
         super().__init__()
         self.num_networks = 0
         self.num_species = 0
