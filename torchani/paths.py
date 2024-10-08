216709796e85c41fdc1040905d3f8926b4ab068a
ipickering <ign.pickering@gmail.com>
2024-10-08 11:29:04 -0400

    Use cli in typer, and fix datasets tests

--- torchani/paths.py
+++ torchani/paths.py
@@ -1,22 +1,38 @@
 r"""
 Default location for various TorchANI resources
 """
+import typing as tp
 import os
 from pathlib import Path
+from torchani.annotations import StrPath
 
-ENV_DATA_DIR = os.getenv("TORCHANI_DATA_DIR")
-
-if ENV_DATA_DIR:
-    DATA_DIR = Path(ENV_DATA_DIR)
-else:
-    DATA_DIR = Path(Path.home(), ".local", "share", "torchani")
+DATA_DIR = Path(Path.home(), ".local", "share", "torchani")
 
 STATE_DICTS = DATA_DIR / "StateDicts"
 DATASETS = DATA_DIR / "Datasets"
 NEUROCHEM = DATA_DIR / "Neurochem"
 
-DATA_DIR.mkdir(exist_ok=True, parents=True)
-STATE_DICTS.mkdir(exist_ok=True, parents=True)
-DATASETS.mkdir(exist_ok=True, parents=True)
-NEUROCHEM.mkdir(exist_ok=True, parents=True)
 RESOURCES = Path(__file__).resolve().parent / "resources"
+
+
+def set_data_dir(data_dir: tp.Optional[StrPath] = None) -> None:
+    global STATE_DICTS, DATASETS, NEUROCHEM
+    if data_dir is None:
+        ENV_DATA_DIR = os.getenv("TORCHANI_DATA_DIR")
+        if ENV_DATA_DIR:
+            DATA_DIR = Path(ENV_DATA_DIR)
+        else:
+            DATA_DIR = Path(Path.home(), ".local", "share", "torchani")
+    else:
+        DATA_DIR = Path(data_dir)
+
+    STATE_DICTS = DATA_DIR / "StateDicts"
+    DATASETS = DATA_DIR / "Datasets"
+    NEUROCHEM = DATA_DIR / "Neurochem"
+    DATA_DIR.mkdir(exist_ok=True, parents=True)
+    STATE_DICTS.mkdir(exist_ok=True, parents=True)
+    DATASETS.mkdir(exist_ok=True, parents=True)
+    NEUROCHEM.mkdir(exist_ok=True, parents=True)
+
+
+set_data_dir()
