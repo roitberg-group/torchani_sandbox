Fix units and add convenient plot function

torchani/potentials/fixed_coulomb.py
@@ -26,10 +26,13 @@ class FixedCoulomb(BasePairPotential):
         self.register_buffer("_charges", torch.tensor(charges), persistent=False)
 
     def pair_energies(self, elem_idxs: Tensor, neighbors: Neighbors) -> Tensor:
+        # Clamp distances to prevent singularities when dividing by zero
+        # All internal calcs use atomic units, so convert to Bohr
+        dists = self.clamp(neighbors.distances) * self.ANGSTROM_TO_BOHR
         elem_pairs = elem_idxs.flatten()[neighbors.indices]
         charge_prod = self._charges[elem_pairs[0]] * self._charges[elem_pairs[1]]
         charge_prod /= self._dielectric
-        return charge_prod / self.clamp(neighbors.distances)
+        return charge_prod / dists
 
 
 # TODO: Trainable?
@@ -63,8 +66,10 @@ class FixedMNOK(BasePairPotential):
         return 2 / (self._eta[elem_pairs[0]] + self._eta[elem_pairs[1]])
 
     def pair_energies(self, elem_idxs: Tensor, neighbors: Neighbors) -> Tensor:
+        # No need to clamp as long as inv_eta ** 2 is nonzero
+        # All internal calcs use atomic units, so convert to Bohr
+        dists = neighbors.distances * self.ANGSTROM_TO_BOHR
         elem_pairs = elem_idxs.flatten()[neighbors.indices]
         inv_eta = self.combine_inv_eta(elem_pairs)
         charge_prod = self._charges[elem_pairs[0]] * self._charges[elem_pairs[1]]
-        #  No need to clamp as long as inv_eta ** 2 is nonzero
-        return charge_prod / (neighbors.distances**2 + inv_eta**2).sqrt()
+        return charge_prod / (dists**2 + inv_eta**2).sqrt()

torchani/potentials/xtb.py
@@ -60,9 +60,8 @@ class RepulsionXTB(BasePairPotential):
         neighbors: Neighbors,
     ) -> Tensor:
         # Clamp distances to prevent singularities when dividing by zero
-        # Also, all internal calculations of this module are made with atomic units,
-        # so distances are first converted to bohr
-        distances = self.clamp(neighbors.distances) * self.ANGSTROM_TO_BOHR
+        # All internal calcs use atomic units, so convert to Bohr
+        dists = self.clamp(neighbors.distances) * self.ANGSTROM_TO_BOHR
 
         # Distances has all interaction pairs within a given cutoff, for a
         # molecule or set of molecules and atom_index12 holds all pairs of
@@ -75,4 +74,4 @@ class RepulsionXTB(BasePairPotential):
         k_rep_ab = self.k_rep_ab[species12[0], species12[1]]
 
         # calculates repulsion energies using distances and constants
-        return (y_ab / distances) * torch.exp(-sqrt_alpha_ab * (distances**k_rep_ab))
+        return (y_ab / dists) * torch.exp(-sqrt_alpha_ab * (dists**k_rep_ab))

torchani/potentials/zbl.py
@@ -7,7 +7,8 @@ from torchani.potentials.core import BasePairPotential
 from torchani.neighbors import Neighbors
 
 
-# Ziegler-Biersack-Littman screened nuclear repulsion
+# Ziegler-Biersack-Littman (ZBL) repulsion potential, which
+# models screened nuclear repulsion
 #  Chapter 2, "Universal screening fn" etc
 # TODO: trainable
 class RepulsionZBL(BasePairPotential):
@@ -15,11 +16,9 @@ class RepulsionZBL(BasePairPotential):
         self,
         symbols: tp.Sequence[str],
         # Potential
-        # one value is from Ziegler et.al., the other from lammps, I suspect they
-        # use different units for coords
-        k: float = 0.8853,  # or 0.46850?
-        phi_coeffs: tp.Sequence[float] = (),
-        phi_exponents: tp.Sequence[float] = (),
+        k: float = 0.8853,  # LAMMPS uses 0.46850 (internally uses angstrom)
+        screen_coeffs: tp.Sequence[float] = (),
+        screen_exponents: tp.Sequence[float] = (),
         eff_exponent: float = 0.23,
         eff_atomic_nums: tp.Sequence[float] = (),
         trainable: tp.Sequence[str] = (),
@@ -34,42 +33,49 @@ class RepulsionZBL(BasePairPotential):
             torch.arange(118, dtype=torch.float).tolist(),
         )
 
-        if not len(phi_exponents) == len(phi_coeffs):
-            raise ValueError("phi_exponents and phi_coeffs must have the same len")
-
+        if not len(screen_exponents) == len(screen_coeffs):
+            raise ValueError(
+                "screen_exponents and screen_coeffs must have the same len"
+            )
         # Defaults from Ziegler et. al.
-        if not phi_exponents:
-            phi_exponents = [0.18175, 0.50986, 0.28022, 0.02817]
-        if not phi_coeffs:
-            phi_coeffs = [3.19980, 0.94229, 0.40290, 0.20162]
+        # Note: In Ziegler et.al. the parameters are:
+        # coeffs: 0.1818, 0.5099 0.2802 0.02817 (typo in a part of the book)
+        # exponents: 3.2, 0.9423, 0.4028, 0.2016
+        # These parameters come directly from the LAMMPS code
+        if not screen_coeffs:
+            # Lammps uses 0.02817 for the last coeff, and that is needed to make
+            # the sum be 1. There is probably a typo in the SRIM book
+            screen_coeffs = [0.18175, 0.50986, 0.28022, 0.02817]
+        if not screen_exponents:
+            screen_exponents = [3.19980, 0.94229, 0.40290, 0.20162]
+
+        if not math.isclose(sum(screen_coeffs), 1.0):
+            raise ValueError("Screen coeffs must sum to 1")
 
         # Params and buffers
         self.register_buffer(
-            "eff_atomic_nums", torch.tensor(eff_atomic_nums), persistent=False
+            "_eff_atomic_nums", torch.tensor(eff_atomic_nums), persistent=False
         )
         self.register_buffer(
-            "_coeffs", torch.tensor(phi_coeffs).view(1, -1), persistent=False
+            "_coeffs", torch.tensor(screen_coeffs).view(1, -1), persistent=False
         )
         self.register_buffer(
-            "_exponents", torch.tensor(phi_exponents).view(1, -1), persistent=False
+            "_exponents", torch.tensor(screen_exponents).view(1, -1), persistent=False
         )
         self._k = k
         self._kz = eff_exponent
 
     def pair_energies(self, elem_idxs: Tensor, neighbors: Neighbors) -> Tensor:
         # Clamp distances to prevent singularities when dividing by zero
-        # Also, all internal calculations of this module are made with atomic units,
-        # so distances are first converted to bohr
+        # All internal calcs use atomic units, so convert to Bohr
         dists = self.clamp(neighbors.distances) * self.ANGSTROM_TO_BOHR
-
         elem_pairs = elem_idxs.flatten()[neighbors.indices]
-        eff_zi = self.eff_atomic_nums[elem_pairs[0]]
-        eff_zj = self.eff_atomic_nums[elem_pairs[1]]
+        eff_za = self._eff_atomic_nums[elem_pairs[0]]
+        eff_zb = self._eff_atomic_nums[elem_pairs[1]]
+        eff_coulomb_term = eff_za * eff_zb / dists
+        reduced_dists = dists * (eff_za**self._kz + eff_zb**self._kz) / self._k
+        screen_factor = self.screen_fn(reduced_dists)
+        return eff_coulomb_term * screen_factor
 
-        prefactor = eff_zi * eff_zj / dists
-        inv_screen_length = (eff_zi**self._kz + eff_zj**self._kz) / self._k
-        reduced_dists = dists * inv_screen_length
-        return prefactor * self._phi(reduced_dists)
-
-    def _phi(self, dists: Tensor) -> Tensor:  # Output shape is same as input
+    def screen_fn(self, dists: Tensor) -> Tensor:  # Output shape is same as input
         return (self._coeffs * torch.exp(-self._exponents * dists.view(-1, 1))).sum(-1)