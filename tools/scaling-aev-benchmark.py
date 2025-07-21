Fix maxatoms_arr for tools/scaling-aev-benchmark.py

tools/scaling-aev-benchmark.py
@@ -581,12 +581,7 @@ if __name__ == "__main__":
     # if run for plots
     if args.plot:
         maxatoms_arr = np.concatenate(
-            [
-                [
-                    300,
-                ],
-                [3000],
-            ]
+            [[100, 3000, 5000, 6000, 8000], np.arange(10000, 31000, 5000)]
         )
         file = "6ZDH.pdb"
         run_for_plot(file, maxatoms_arr, nnp_ref, nnp_cuaev)