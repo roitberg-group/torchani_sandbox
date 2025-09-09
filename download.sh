dea84805f03e418a321a819c1e65876370afe9b7
ipickering <ign.pickering@gmail.com>
2025-09-08 21:32:49 -0400

    Stop relying on dropbox

--- download.sh
+++ download.sh
@@ -2,6 +2,6 @@
 
 # Download the data
 echo "Downloading data ..."
-wget --no-verbose https://www.dropbox.com/sh/2c8zdqc1hrqsgwy/AAD6l24ngoiFa6DRapF6HPk5a/ -O download.zip
+wget --no-verbose https://huggingface.co/datasets/roitberg-group/torchani-tests-pickled-files/resolve/main/download.zip
 echo "Extracting data ..."
 unzip -q download.zip -d download || [[ $? == 2 ]]  # unzip return 2 for dropbox created zip file
