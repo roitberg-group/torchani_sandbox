8b02690a6738fc21ff86d010256136aaa6ce9927
ipickering <ign.pickering@gmail.com>
2025-09-09 00:21:42 -0400

    Update dev data file

--- download-dev-data.sh
+++ download-dev-data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Download the data
+mkdir dev-data
 echo "Downloading data ..."
 wget --no-verbose https://huggingface.co/datasets/roitberg-group/torchani-tests-pickled-files/resolve/main/download.zip
 echo "Extracting data ..."
